13681333801c712c68cb21cf6031cf6e6eed820d
SC <sc4400@columbia.edu>
2020-03-14 20:20:00 -0400

    Add Leetcode 114 (binary tree preorder traversal, iterative)

---
+++ 114-Flatten Binary Tree to Linked List.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def flatten(self, root):
+        """
+        :type root: TreeNode
+        :rtype: None Do not return anything, modify root in-place instead.
+        """
+        if root is None:
+            return
+# binary tree preorder traversal (iterative)
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+                
+            node.left = None
+            if stack:
+                node.right = stack[-1]
+            else:
+                node.right = None
