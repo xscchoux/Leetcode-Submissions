6220d3d30c02c0668bfbedb194610ea3adc82d81
SC <sc4400@columbia.edu>
2020-03-16 20:19:52 -0400

    add Leetcode 742 (DFS to construct a graph, BFS to search leaf node)

---
+++ 742-Closest Leaf in a Binary Tree.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+from collections import defaultdict, deque
+class Solution(object):
+    def findClosestLeaf(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+
+# construct the graph
+        graph = defaultdict(list)
+        queue = deque([])
+        def dfs(root, par=None):
+            if not root:
+                return
+            if root.val == k:
+                queue.append(root)
+            graph[par].append(root)
+            graph[root].append(par)
+            dfs(root.left, root)
+            dfs(root.right, root)
+        dfs(root)   
+        
+        seen = set(queue)
+        while queue:
+            size = len(queue)
+            node = queue.popleft()
+            if node:                    # note that there would be "None" nodes
+                if len(graph[node]) <= 1:  # note that root has a childNode "None"
+                    return node.val
+                for nei in graph[node]:
+                    if nei not in seen:
+                        seen.add(nei)
+                        queue.append(nei)
