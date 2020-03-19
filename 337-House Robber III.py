d5c9979d1441d3ccfde3705a6b6e38ea196ef8bf
SC <sc4400@columbia.edu>
2020-03-19 17:54:23 -0400

    add Leetcode 337 (DFS)

---
+++ 337-House Robber III.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def rob(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        
+# DFS + memo  Time complexity: O(N), space complexity:O(N) 
+        memo = {}
+        
+        def dfs(root):
+            if not root:
+                return 0
+            if root in memo:
+                return memo[root]
+            
+            value = 0
+            if root.left:
+                value += dfs(root.left.left) + dfs(root.left.right)
+            if root.right:
+                value += dfs(root.right.left) + dfs(root.right.right)
+            
+            memo[root] = max(root.val + value, dfs(root.left)+dfs(root.right))
+            return memo[root]
+        
+        return dfs(root)
+
+# without memo
+        def dfs(root):
+            if not root:
+                return [0,0]
+            
+            leftRob, leftNoRob = dfs(root.left)
+            rightRob, rightNoRob = dfs(root.right)
+            
+            return max(root.val + leftNoRob + rightNoRob, leftRob+rightRob), leftRob + rightRob
+        
+        return max(dfs(root))
