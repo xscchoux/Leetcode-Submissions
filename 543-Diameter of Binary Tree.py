83d56ce84e14674096a9eb90c1caf779c938e9d8
SC <sc4400@columbia.edu>
2020-04-11 20:37:55 -0400

    add Leetcode 543 (tree)

---
+++ 543-Diameter of Binary Tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def diameterOfBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        self.res = 0
+        def dfs(root):
+            if not root:
+                return 0
+            
+            left = dfs(root.left)
+            right = dfs(root.right)
+            self.res = max(self.res, left+right)
+            return max(left, right) + 1
+        
+        dfs(root)
+        return self.res
