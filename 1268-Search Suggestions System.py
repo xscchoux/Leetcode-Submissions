4d62281f083d63b5ad5ce214a67e74070374a484
SC <sc4400@columbia.edu>
2020-04-12 18:28:30 -0400

    add Leetcode 1268 (Trie)

---
+++ 1268-Search Suggestions System.py
@@ -0,0 +1,49 @@
+class TrieNode(object):
+    def __init__(self):
+        self.children = {}
+        self.word = []
+        
+class Trie(object):
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def insert(self, word):
+        node = self.root
+        for w in word:
+            if w not in node.children:
+                node.children[w] = TrieNode()
+            node = node.children[w]
+            node.word.append(word)
+    
+    def search(self, searchWord):
+        node = self.root
+        ans = []
+        for i in range(len(searchWord)):
+            if searchWord[i] in node.children:
+                node = node.children[searchWord[i]]
+                ans.append(sorted(node.word)[:3])
+            else:
+                break
+        
+        for i in range(len(searchWord) - len(ans)):
+            ans.append([])
+        return ans
+
+class Solution(object):
+    def suggestedProducts(self, products, searchWord):
+        """
+        :type products: List[str]
+        :type searchWord: str
+        :rtype: List[List[str]]
+        """
+
+        trie = Trie()
+# insert products
+# O(nk),  n:num of products, k: length of the product
+        for product in products:
+            trie.insert(product)
+
+# searcg products
+# O(k*nlogn)
+        res = trie.search(searchWord)
+        return res
