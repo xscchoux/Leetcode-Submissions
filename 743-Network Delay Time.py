dccd8f11b195c3bf936a21f0ba8f223d65b8fba8
SC <sc4400@columbia.edu>
2020-04-17 03:11:57 -0400

    add Leetcode 743 (DFS, can also be solved using Dijkstra)

---
+++ 743-Network Delay Time.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+class Solution(object):
+    def networkDelayTime(self, times, N, K):
+        """
+        :type times: List[List[int]]
+        :type N: int
+        :type K: int
+        :rtype: int
+        """
+        dist= {node: float('inf') for node in range(1,N+1)}
+        
+        graph = defaultdict(list)
+        
+        for u, v, w in times:
+            graph[u].append((w,v))
+            
+        def dfs(K, elapsed):
+            if dist[K] <= elapsed:
+                return
+            dist[K] = elapsed
+            for child in sorted(graph[K]):
+                dfs(child[1], elapsed+child[0])
+        
+        dfs(K, 0)
+        res = max(dist.values())
+        
+        return -1 if res == float('inf') else res
