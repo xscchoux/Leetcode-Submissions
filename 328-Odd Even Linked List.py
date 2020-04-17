00cea7035f9feca1e662e07e0116ff745e7d8dd2
SC <sc4400@columbia.edu>
2020-04-17 03:41:04 -0400

    modify Leetcode 328, and constant space solution

--- 328-Odd Even Linked List.py
+++ 328-Odd Even Linked List.py
@@ -10,23 +10,39 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
+# with extra space
+#         if not head:
+#             return None
+#         head1 = oddHead = ListNode(0) 
+#         head2 = evenHead = ListNode(0)
+        
+#         isOdd = True
+        
+#         while head:
+#             if isOdd:
+#                 oddHead.next = head
+#                 oddHead = oddHead.next
+#             else:
+#                 evenHead.next = head
+#                 evenHead = evenHead.next
+#             head = head.next
+#             isOdd = not isOdd
+        
+#         evenHead.next = None
+#         oddHead.next = head2.next
+#         return head1.next
+
+# without extra space
         if not head:
             return None
-        head1 = oddHead = ListNode(0) 
-        head2 = evenHead = ListNode(0)
+        oddHead, evenHead = head, head.next
+        odd, even = oddHead, evenHead
+        while even and even.next:
+            odd.next = even.next
+            odd = even.next
+            even.next = odd.next
+            even = odd.next
+            
+        odd.next = evenHead
         
-        isOdd = True
-        
-        while head:
-            if isOdd:
-                oddHead.next = head
-                oddHead = oddHead.next
-            else:
-                evenHead.next = head
-                evenHead = evenHead.next
-            head = head.next
-            isOdd = not isOdd
-        
-        evenHead.next = None
-        oddHead.next = head2.next
-        return head1.next
+        return oddHead
