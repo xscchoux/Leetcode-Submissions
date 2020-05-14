d36c859a5bee4ec238708ef46650a108581a63dc
SC <sc4400@columbia.edu>
2020-05-14 16:34:18 -0400

    modify Leetcode 55, add another method

--- 55-Jump Game.py
+++ 55-Jump Game.py
@@ -5,13 +5,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
+# O(n)
         if not nums or len(nums) <= 1:
             return True
-        
         lastpos = len(nums) - 1
-      
         for i in range(len(nums)-1, -1, -1):
             if i + nums[i] >= lastpos:
                 lastpos = i
 
         return lastpos == 0
+    
+# method 2
+        _max = 0
+        for i in range(len(nums)):
+            if i > _max:
+                return False
+            _max = max(_max, i + nums[i])
+            
+        return True
