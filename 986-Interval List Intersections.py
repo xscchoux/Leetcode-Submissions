5961b61da1e54d62df76966bde543e3aa81b8b1a
SC <sc4400@columbia.edu>
2020-05-23 22:03:15 -0400

    add Leetcode 986

---
+++ 986-Interval List Intersections.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def intervalIntersection(self, A, B):
+        """
+        :type A: List[List[int]]
+        :type B: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        res = []
+        i,j = 0, 0
+        while i < len(A) and j <len(B):
+            start = max(A[i][0], B[j][0])
+            end = min(A[i][1], B[j][1])
+            if start <= end:
+                res.append([start, end])
+                
+            if A[i][1] < B[j][1]:
+                i += 1
+            else:
+                j += 1
+        
+        return res
