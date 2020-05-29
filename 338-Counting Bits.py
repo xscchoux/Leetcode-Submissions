2b09df0acf21a96a204570d1ed2d3721c1ac089f
SC <sc4400@columbia.edu>
2020-05-28 22:51:41 -0400

    add Leetocde 338

---
+++ 338-Counting Bits.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def countBits(self, num):
+        """
+        :type num: int
+        :rtype: List[int]
+        """
+        res = [0]*(num+1)
+        
+        for i in range(1,num+1):
+            if i%2 == 0:
+                res[i] = res[i//2]
+            else:
+                res[i] = res[i-1] + 1
+                
+        return res
