d85e33a1988aab92e0e9a9008a866cb5a211f445
SC <sc4400@columbia.edu>
2021-03-09 18:41:13 -0500

    add Leetcode 145, 144(JS)

---
+++ 145-Binary Tree Postorder Traversal.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return None
+        
+        stack = [root]
+        res = deque([])
+        
+        while stack:
+            node = stack.pop()
+            res.appendleft(node.val)
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        
+        return res
