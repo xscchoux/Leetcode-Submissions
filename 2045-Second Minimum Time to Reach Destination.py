6453882cc552a2609413ba1a3d6f56dbd01dc16f
SC <sc4400@columbia.edu>
2021-10-20 12:12:06 -0500

    add Leetocde 2045 (hard Dijkstra problem)

---
+++ 2045-Second Minimum Time to Reach Destination.py
@@ -0,0 +1,37 @@
+from heapq import heappush, heappop
+class Solution(object):
+    def secondMinimum(self, n, edges, time, change):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type time: int
+        :type change: int
+        :rtype: int
+        """
+        graph = collections.defaultdict(list)
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        
+        elapsed = [float('inf')]*(n+1)
+        freq = [0]*(n+1)
+        change2 = 2*change
+        pq = [(0, 1)]  # time, vertex
+        found = False
+        
+        while pq:
+            currT, currV = heappop(pq)
+            quo = currT//change
+            if quo%2:
+                currT += (change2 - currT%change2)
+            for nxt in graph[currV]:
+                if currT + time != elapsed[nxt] and freq[nxt] < 2:
+                    if nxt == n:
+                        if not found:
+                            found = True
+                        else:
+                            return currT+time
+                    freq[nxt] += 1
+                    elapsed[nxt] = currT + time
+                    heappush(pq, (currT+time,nxt) )
+                    
