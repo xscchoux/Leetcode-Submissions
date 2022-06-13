3ba05a43e862d8d993e8a00b08d245ff72f2546c
Chou, Shao-Chi <shao-chi.chou@fujitsu.com>
2022-06-13 03:11:19 -0500

    add Leetcode 2306(tricky greedy)

--- 2306-Naming a Company.py
+++ 2306-Naming a Company.py
@@ -6,21 +6,19 @@ class Solution(object):
         """
 # https://www.youtube.com/watch?v=DuUxLAikrIE
         hmap = collections.defaultdict(set)
-        
+        string = set()
         for idea in ideas:
             hmap[idea[0]].add(idea[1:])
-        
-        string ="abcdefghijklmnopqrstuvwxyz"
+            string.add(idea[0])
+
         res = 0
-        
-        for i in range(26):
-            for j in range(i+1, 26):
-                l1, l2 = len(hmap[string[i]]), len(hmap[string[j]])
-                if l1 == 0 or l2 == 0:
-                    continue
+        arr = list(string)
+        for i in range(len(arr)):
+            for j in range(i+1, len(arr)):
+                l1, l2 = len(hmap[arr[i]]), len(hmap[arr[j]])
                 common = 0
-                for s in hmap[string[j]]:
-                    if s in hmap[string[i]]:
+                for s in hmap[arr[j]]:
+                    if s in hmap[arr[i]]:
                         common += 1
                 res += (l1-common)*(l2-common)*2
                 
