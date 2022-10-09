bbac8f611577ac78510a3a9670375a3f5ee8b1e3
Chou, Shao-Chi <shao-chi.chou@fujitsu.com>
2022-10-09 17:05:27 -0500

    modified Leetcode 716 (added a SortedList solution)

--- 716-Max Stack.py
+++ 716-Max Stack.py
@@ -61,3 +61,58 @@ class MaxStack(object):
 # param_3 = obj.top()
 # param_4 = obj.peekMax()
 # param_5 = obj.popMax()
+
+
+
+from sortedcontainers import SortedList
+class MaxStack(object):
+
+    def __init__(self):
+        self.cnt = 0
+        self.maxStack = SortedList()
+        self.stack = SortedList()
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.stack.add((self.cnt, x))
+        self.maxStack.add((x, self.cnt))
+        self.cnt += 1
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        count, val = self.stack.pop()
+        self.maxStack.remove((val, count))
+        return val
+        
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1][1]
+
+    def peekMax(self):
+        """
+        :rtype: int
+        """
+        return self.maxStack[-1][0]        
+
+    def popMax(self):
+        """
+        :rtype: int
+        """
+        val, count = self.maxStack.pop()
+        self.stack.remove((count, val))
+        return val
+
+# Your MaxStack object will be instantiated and called as such:
+# obj = MaxStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.peekMax()
+# param_5 = obj.popMax()
