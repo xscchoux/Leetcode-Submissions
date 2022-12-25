e717a9fc76bdbd317694efd56503f9697b02eefa
Chou, Shao-Chi <shao-chi.chou@fujitsu.com>
2022-12-25 04:21:05 -0600

    modify Leetcode 790 (add one more answer)

--- 790-Domino and Tromino Tiling.py
+++ 790-Domino and Tromino Tiling.py
@@ -5,14 +5,33 @@ class Solution(object):
         :rtype: int
         """
 # https://www.youtube.com/watch?v=S-fUTfqrdq8
-
+        
+# O(n) space
+        full = [0]*(n+1)
+        partial = [0]*(n+1)
+        
+        full[0] = 1
+        full[1] = 1
+        
         kMod = 1000000007
-        dp =[[0]*2 for _ in range(n+1)]
-        dp[0][0] = 1
-        dp[1][0] = 1
         
         for i in range(2, n+1):
-            dp[i][0] = (dp[i-1][0] + dp[i-2][0] + 2*dp[i-1][1])%kMod
-            dp[i][1] = (dp[i-2][0] + dp[i-1][1])%kMod
-                        
-        return dp[n][0]
+            full[i] = (full[i-1] + full[i-2] + 2*partial[i-1])%kMod
+            partial[i] = (partial[i-1] + full[i-2])%kMod
+        
+        return full[-1]
+    
+# O(1) space  
+        f_curr = 1
+        f_prev = 1
+        p_curr = 0
+        
+        kMod = 1000000007
+        
+        for i in range(2, n+1):
+            tmp = f_curr
+            f_curr = (f_curr + f_prev + 2*p_curr)%kMod
+            p_curr = (p_curr + f_prev)%kMod
+            f_prev = tmp
+            
+        return f_curr
