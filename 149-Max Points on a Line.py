5fc45cdac4763ec34195a7a9bff2861fcd9f3b85
Chou, Shao-Chi <shao-chi.chou@fujitsu.com>
2023-01-08 01:39:40 -0600

    add another solution for Leetcode 149

--- 149-Max Points on a Line.py
+++ 149-Max Points on a Line.py
@@ -26,3 +26,15 @@ class Solution(object):
             res = max(res, duplicate + max(count.values()))
         
         return res
+
+# another (better?) solution
+        points.sort(key = lambda x:(x[0], x[1]))
+        dp = [collections.defaultdict(int) for _ in range(len(points))]
+        res = 0
+        
+        for i in range(1, len(points)):
+            for j in range(i):
+                arc = math.atan2(points[j][1] - points[i][1], points[j][0] - points[i][0])
+                dp[i][arc] = dp[j][arc] + 1
+                res = max(res, dp[i][arc])
+        return res+1
