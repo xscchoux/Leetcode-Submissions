030e37710548d7ed71e9087a0dadf2c77f15c8b4
Chou, Shao-Chi <shao-chi.chou@fujitsu.com>
2023-02-09 23:44:31 -0600

    update Leetcode 1162

--- 1162-As Far from Land as Possible.py
+++ 1162-As Far from Land as Possible.py
@@ -31,3 +31,33 @@ class Solution(object):
             land = newLand
         
         return count-1
+
+
+# Updated solution
+lass Solution:
+    def maxDistance(self, grid: List[List[int]]) -> int:
+        N = len(grid)
+        res = -1
+        queue = collections.deque([])
+        visited = [[0]*N for _ in range(N)]
+        distance = -1
+        
+        for i in range(N):
+            for j in range(N):
+                if grid[i][j] == 1:
+                    queue.append((i, j))
+        
+        if len(queue) == 0 or len(queue) == N*N:
+            return -1
+                    
+        while queue:
+            for _ in range(len(queue)):
+                currR, currC = queue.popleft()
+                for dr, dc in ((1, 0), (0, 1), (-1, 0), (0, -1)):
+                    nxtR, nxtC = currR+dr, currC+dc
+                    if 0<=nxtR<N and 0<=nxtC<N and grid[nxtR][nxtC] == 0 and visited[nxtR][nxtC] == 0:
+                        visited[nxtR][nxtC] = 1
+                        queue.append((nxtR, nxtC))
+            distance += 1
+        
+        return distance
