0a57e859f22fd7081a51eff2484304a9f685d2c4
Chou, Shao-Chi <shao-chi.chou@fujitsu.com>
2023-03-01 01:54:40 -0600

    add top-down solution for Leetcode 72

--- 72-Edit Distance.py
+++ 72-Edit Distance.py
@@ -1,25 +1,36 @@
-class Solution(object):
-    def minDistance(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: int
-        """
-        l1 = len(word1)
-        l2 = len(word2)
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+    
+# top down
+        @cache
+        def dp(l1, l2):
+            if l1 < 0 or l2 < 0:
+                if l1 < 0:
+                    return l2 + 1
+                else:
+                    return l1 + 1
+            if word1[l1] == word2[l2]:
+                return dp(l1-1, l2-1)
+            else:
+                return 1 + min(dp(l1-1, l2-1), dp(l1-1, l2), dp(l1, l2-1))
         
+        return dp(len(word1)-1, len(word2)-1)
+    
+# bottom up    
+        l1, l2 = len(word1), len(word2)
         dp = [[0]*(l2+1) for _ in range(l1+1)]
         
         for j in range(l2+1):
             dp[0][j] = j
-        
+            
         for i in range(l1+1):
             dp[i][0] = i
-        
+            
         for i in range(1, l1+1):
             for j in range(1, l2+1):
                 if word1[i-1] == word2[j-1]:
                     dp[i][j] = dp[i-1][j-1]
                 else:
-                    dp[i][j] = 1 + min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1])
+                    dp[i][j] = 1 + min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) # replace, delete, insert
+        
         return dp[-1][-1]
