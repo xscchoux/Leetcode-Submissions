106dce562f7b63523e4b174261fb4e47c2db2443
Chou, Shao-Chi <shao-chi.chou@fujitsu.com>
2023-03-11 17:49:04 -0600

    add Leetocde 109 (add a better solution)

--- 109-Convert Sorted List to Binary Search Tree.py
+++ 109-Convert Sorted List to Binary Search Tree.py
@@ -1,34 +1,58 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def sortedListToBST(self, head):
-        """
-        :type head: ListNode
-        :rtype: TreeNode
-        """
-        output = []
+class Solution:
+    def sortedListToBST(self, head: Optional[ListNode]) -> Optional[TreeNode]:
+        
+        arr = []
         while head:
-            output.append(head.val)
+            arr.append(head.val)
             head = head.next
 
-        def dfs(output):
-            if not output:
-                return None
-            lo, hi = 0, len(output)-1
-            mid = (lo+hi)//2
-            root = TreeNode(output[mid]) 
-            root.left = dfs(output[:mid])
-            root.right = dfs(output[mid+1:])
-            return root
-            
+        l, r = 0, len(arr)-1
         
-        return dfs(output)
+        def dfs(left, right):
+            if left > right:
+                return None
+            mid = left + (right-left)//2
+            curr = ListNode(arr[mid])
+            curr.left = dfs(left, mid-1)
+            curr.right = dfs(mid+1, right)
+            
+            return curr
+        
+        return dfs(l, r)
+
+
+# better
+        p = head
+        length = 0
+        while p:
+            p = p.next
+            length += 1
+    
+        self.head = head
+        
+        def dfs(left, right):
+            if right < left:
+                return None
+            mid = left + (right-left)//2
+            leftNode = dfs(left, mid-1)
+            
+            curr = ListNode(self.head.val)
+            curr.left = leftNode
+            self.head = self.head.next
+            
+            rightNode = dfs(mid+1, right)
+            curr.right = rightNode
+            return curr
+        
+        return dfs(0, length-1)
