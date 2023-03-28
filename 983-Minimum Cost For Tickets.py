3f75572e1f9141b8e2bc1db630367eaa081db117
Chou, Shao-Chi <shao-chi.chou@fujitsu.com>
2023-03-27 23:21:32 -0500

    update Leetcode 983 (add bottom-up DP solution)

--- 983-Minimum Cost For Tickets.py
+++ 983-Minimum Cost For Tickets.py
@@ -52,3 +52,17 @@ class Solution(object):
             return res
         
         return dp(0)
+
+
+# better
+        mx = days[-1]
+        setDay = set(days)
+        dp = [0]*(mx+1)
+        
+        for i in range(1, mx+1):
+            if i in setDay:
+                dp[i] = min(dp[i-1] + costs[0], dp[max(i-7, 0)] + costs[1], dp[max(i-30, 0)] + costs[2])
+            else:
+                dp[i] = dp[i-1]
+
+        return dp[-1]
