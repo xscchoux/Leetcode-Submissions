51bbca9f4277b6274ede8ffa0eb36fae4a47df39
Chou, Shao-Chi <shao-chi.chou@fujitsu.com>
2023-04-05 01:09:14 -0500

    update Leetcode 2439 (add a solution using walrus operator)

--- 2439-Minimize Maximum of Array.py
+++ 2439-Minimize Maximum of Array.py
@@ -12,3 +12,16 @@ class Solution(object):
             mx = max(mx, (curr+i)//(i+1))
             
         return mx
+
+
+# walrus operator
+class Solution:
+    def minimizeArrayValue(self, nums: List[int]) -> int:
+        res = tot = 0
+        
+        for i in range(len(nums)):
+            tot += nums[i]
+            if (x:=math.ceil(tot/(i+1))) > res:
+                res = x
+        
+        return res
