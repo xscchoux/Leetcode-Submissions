3b2c71db7b3d4fda5143b02c6391c0cc54f0a827
Chou, Shao-Chi <shao-chi.chou@fujitsu.com>
2023-04-06 02:34:07 -0500

    update Leetcode 2300 (add solution using cache)

--- 2300-Successful Pairs of Spells and Potions.py
+++ 2300-Successful Pairs of Spells and Potions.py
@@ -24,3 +24,36 @@ class Solution(object):
                 res[i] = len(potions) - right
                 
         return res
+
+
+
+
+# add cache
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        potions.sort()
+        res = []
+        cache = dict()
+        
+        for num in spells:
+            if num in cache:
+                res.append(cache[num])
+                continue
+
+            left, right = 0, len(potions)-1
+            curr = 0
+            while left + 1 < right:
+                mid = left + (right-left)//2
+                if num*potions[mid] >= success:
+                    right = mid
+                else:
+                    left = mid
+            if num*potions[left] >= success:
+                curr = len(potions) - left
+            elif num*potions[right] >= success:
+                curr = len(potions) - right
+                
+            cache[num] = curr
+            res.append(curr)
+            
+        return res
