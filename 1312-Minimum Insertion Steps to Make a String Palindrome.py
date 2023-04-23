86c5d948454c4546e051929c616c26748ae2969a
Chou, Shao-Chi <shao-chi.chou@fujitsu.com>
2023-04-22 19:12:53 -0500

    Update Leetcode 1312 (add Bottom-Up DP with space optimization, very hard to come up with)

--- 1312-Minimum Insertion Steps to Make a String Palindrome.py
+++ 1312-Minimum Insertion Steps to Make a String Palindrome.py
@@ -37,3 +37,58 @@ class Solution(object):
                 else:
                     dp[i][j] = min(dp[i][j-1], dp[i+1][j]) + 1
         return dp[0][len(s)-1]
+
+
+
+
+# more solutions...
+
+class Solution:
+    def minInsertions(self, s: str) -> int:
+# Top-Down DP        
+        @cache
+        def dfs(left, right):
+            if left == right:
+                return 1
+            if left > right:
+                return 0
+            
+            if s[left] == s[right]:
+                return 2 + dfs(left+1, right-1)
+            else:
+                return max(dfs(left+1, right), dfs(left, right-1))
+        
+        return len(s) - dfs(0, len(s)-1)
+        
+        
+# Bottom-Up DP
+        N = len(s)
+        dp = [[0]*N for _ in range(N)]
+        for i in range(N):
+            dp[i][i] = 1
+        
+        for i in range(N-1, -1, -1):
+            for j in range(i+1, N):
+                if s[i] == s[j]:
+                    dp[i][j] = 2 + dp[i+1][j-1]
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1])
+        
+        return N - dp[0][len(s)-1]
+
+
+# Bottom-Up DP with reduced space complexity, very hard to come up with
+        N = len(s)
+        dp = [1]*N
+        
+        for i in range(N-1, -1, -1):
+            pre = 0
+            for j in range(i+1, N):
+                tmp = dp[j]  # store dp[i+1][j-1] in tmp
+                if s[i] == s[j]:
+                    dp[j] = 2 + pre
+                else:
+                    dp[j] = max(dp[j], dp[j-1])
+                pre = tmp
+
+        return N - dp[-1]
