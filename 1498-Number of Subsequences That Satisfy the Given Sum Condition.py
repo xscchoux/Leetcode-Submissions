23ec11d75cceff32d39d3df6063816fdd44d3173
Chou, Shao-Chi <shao-chi.chou@fujitsu.com>
2023-05-06 03:41:39 -0500

    update Leetcode 1498 (add a solution using binary search)

--- 1498-Number of Subsequences That Satisfy the Given Sum Condition.py
+++ 1498-Number of Subsequences That Satisfy the Given Sum Condition.py
@@ -1,23 +1,30 @@
-class Solution(object):
-    def numSubseq(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
+class Solution:
+    def numSubseq(self, nums: List[int], target: int) -> int:
+        
+# sort + binary search
         nums.sort()
-        n = len(nums)
-        j = n-1
+        kMod = 1000000007
         res = 0
-        kMod = 10**9 + 7
         
+        for i in range(len(nums)):
+            if nums[i]*2 > target:
+                break
+            ind = bisect_left(nums, target-nums[i]+1)
+            res = (res + pow(2, ind-i-1, kMod))%kMod
         
-        for i in range(n):
-            if 2*nums[i] > target:
+        return res
+    
+# sort + two pointer
+        nums.sort()
+        j = len(nums) - 1
+        kMod = 1000000007
+        res = 0
+        
+        for i in range(len(nums)):
+            if nums[i]*2 > target:
                 break
             while nums[i] + nums[j] > target:
-                j-=1
-                
-            res += pow(2, j - i, kMod)
-        
-        return res%kMod
+                j -= 1
+            res = (res + pow(2, j-i, kMod))%kMod
+            
+        return res
