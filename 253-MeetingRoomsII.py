ab0d79e0ce78fa4dbd10f42e9eb1cb15545bd89e
Chou, Shao-Chi <shao-chi.chou@fujitsu.com>
2023-05-21 22:49:23 -0500

    update Leetcode 253 (add sweep line solution)

--- 253-MeetingRoomsII.py
+++ 253-MeetingRoomsII.py
@@ -1,18 +1,34 @@
-import heapq
-class Solution(object):
-    def minMeetingRooms(self, intervals):
-        """
-        :type intervals: List[List[int]]
-        :rtype: int
-        """
-        if not intervals:
-            return 0
-        intervals.sort()
-        minheap = [intervals[0][1]]
+from heapq import heappush, heappop
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         
-        for i in range(1, len(intervals)):
-            if intervals[i][0] >= minheap[0]:
-                heapq.heappop(minheap)
-            heapq.heappush(minheap,intervals[i][1])
+# sweep line
+        arr = []
+        curr = res = 0
         
-        return len(minheap)
+        for interval in intervals:
+            arr.append((interval[0], 1))
+            arr.append((interval[1], -1))
+        
+        arr.sort(key = lambda x:(x[0], x[1]))
+        
+        for i in range(len(arr)):
+            if arr[i][1] == 1:
+                curr += 1
+            else:
+                curr -= 1
+            res = max(res, curr)
+            
+        return res
+    
+# heap, beautiful solution
+        intervals.sort(key = lambda x:x[0])
+        pq = []
+        
+        for interval in intervals:
+            if pq and interval[0] >= pq[0]:
+                heappop(pq)
+                
+            heappush(pq, interval[1])
+        
+        return len(pq)
