da9c6ae71f1ba1bd57e5f65991c06eba40f23635
Chou, Shao-Chi <shao-chi.chou@fujitsu.com>
2023-06-10 03:34:24 -0500

    update Leetocde 59 (add a more concise solution)

--- 59-Spiral Matrix II.py
+++ 59-Spiral Matrix II.py
@@ -14,25 +14,44 @@ class Solution(object):
             up += 1
             if up > down:
                 break
-
-            for u in range(up, down+1):
-                res[u][right] = count
+            
+            for r in range(up, down+1):
+                res[r][right] = count
                 count += 1
             right -= 1
             if right < left:
                 break
                 
-            for r in range(right, left-1, -1):
-                res[down][r] = count
+            for c in range(right, left-1, -1):
+                res[down][c] = count
                 count += 1
             down -= 1
             if down < up:
                 break
                 
-            for d in range(down, up-1, -1):
-                res[d][left] = count
+            for r in range(down, up-1, -1):
+                res[r][left] = count
                 count += 1
             left += 1
             if left > right:
                 break
+                
+        return res
+
+# Better
+        res = [[0]*n for _ in range(n)]
+        count = 1
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        r = c = 0
+        d = 0
+        
+        while count <= n*n:
+            res[r][c] = count
+            count += 1
+            nxtR, nxtC = r + directions[d][0], c + directions[d][1]
+            if nxtR < 0 or nxtR == n or nxtC < 0 or nxtC == n or res[nxtR][nxtC] != 0:
+                d = (d+1)%4
+            r += directions[d][0]
+            c += directions[d][1]
+
         return res
