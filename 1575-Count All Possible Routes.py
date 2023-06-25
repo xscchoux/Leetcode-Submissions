cce906d53bf572acaf2321e23dac57abe15bd380
Chou, Shao-Chi <shao-chi.chou@fujitsu.com>
2023-06-25 18:08:03 -0500

    update Leetcode 1575 (a little faster)

--- 1575-Count All Possible Routes.py
+++ 1575-Count All Possible Routes.py
@@ -1,20 +1,21 @@
 class Solution:
     def countRoutes(self, locations: List[int], start: int, finish: int, fuel: int) -> int:
-        kMod = 1000000007
-        @cache
-        def dfs(curr, remain):
-            res = 0
-            if curr == finish:
-                res += 1
-            for i in range(len(locations)):
-                if curr == i:
-                    continue
-                diff = abs(locations[i]-locations[curr])
-                if remain >= diff:
-                    res += dfs(i, remain-diff)
-            # if abs(locations[curr]-locations[finish]) <= remain:
-            #     res += 1
-            
-            return res%kMod
+        kMod = 10**9+7
         
-        return dfs(start, fuel)
+        @cache
+        def state(idx, fuel):
+            if idx == finish:
+                tot = 1
+            else:
+                tot = 0
+            
+            for i in range(len(locations)):
+                if i == idx:
+                    continue
+                diff = abs(locations[idx] - locations[i])
+                if fuel >= diff:
+                    tot = (tot + state(i, fuel - diff))%kMod
+            
+            return tot
+        
+        return state(start, fuel)
