d5344f9f251a08dffd287df42144ab50d7be28e8
Chou, Shao-Chi <shao-chi.chou@fujitsu.com>
2023-06-28 01:43:11 -0500

    update Leetcode 1514 (add Bellman-Ford algorithm)

--- 1514-Path with Maximum Probability.py
+++ 1514-Path with Maximum Probability.py
@@ -1,25 +1,49 @@
-from heapq import *
+# Bellman-Ford, O(n*m), m:number of edges
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
-        graph = collections.defaultdict(list)
-        for (u, v), p in zip(edges, succProb):
-            graph[u].append([v, p])
-            graph[v].append([u, p])
-
-        prob = [0]*n
-        visited = set()
+        maxProb = [0]*n
+        maxProb[start] = 1
         
-        # Dijkstra's algorithm
-        queue = [(-1, start)] #weight, index
-        while queue:
-            [currProb, currIdx] = heappop(queue)
-            if currIdx == end:
-                return -currProb
-            currProb = -1*currProb
-
-            for nxt, p in graph[currIdx]:
-                if currProb*p > prob[nxt]:
-                    prob[nxt] = currProb*p
-                    heappush(queue, (-currProb*p, nxt))
- 
-        return prob[end]
+        for _ in range(n-1):
+            update = False
+            for i in range(len(edges)):
+                u, v = edges[i]
+                if maxProb[u] < maxProb[v]*succProb[i]:
+                    maxProb[u] = maxProb[v]*succProb[i]
+                    update = True
+                if maxProb[v] < maxProb[u]*succProb[i]:
+                    maxProb[v] = maxProb[u]*succProb[i]
+                    update = True
+            if not update:
+                break
+        
+        return maxProb[end]
+        
+# Dijkstra's algorithm
+from heapq import heappush, heappop
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
+        prob = [0]*n
+        
+        graph = defaultdict(list)
+        for i in range(len(edges)):
+            u, v = edges[i]
+            w = succProb[i]
+            graph[u].append((v, w))
+            graph[v].append((u, w))
+            
+        pq = [(-1, start)]  # probabilitiy
+        
+        while pq:
+            p, idx = heappop(pq)
+            if prob[idx] != 0:
+                continue
+            if idx == end:
+                return -p
+            prob[idx] = -p
+            for nxt, weight in graph[idx]:
+                if prob[nxt] != 0:
+                    continue
+                heappush(pq, (p*weight, nxt) )
+                
+        return 0
