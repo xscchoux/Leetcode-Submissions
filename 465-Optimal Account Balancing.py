f33c908709cabe21df5e0c45adbe0b473456c493
Chou, Shao-Chi <shao-chi.chou@fujitsu.com>
2023-07-01 21:15:27 -0500

    update Leetcode 465 (add another solution, same logic)

--- 465-Optimal Account Balancing.py
+++ 465-Optimal Account Balancing.py
@@ -1,42 +1,69 @@
-from collections import defaultdict
-class Solution(object):
-    def minTransfers(self, transactions):
-        """
-        :type transactions: List[List[int]]
-        :rtype: int
-        """
+class Solution:
+    def minTransfers(self, transactions: List[List[int]]) -> int:
+        
+# method 1, O( (n-1)! )
+        hmap = defaultdict(int)
+        for u, v, w in transactions:
+            hmap[u] -= w
+            hmap[v] += w
 
-# time complexity: O (m + (N-1)!), m: length of transactions, N: number of people
+        buckets = [v for v in hmap.values() if v]
+        
+        def dfs(arr):
+            if not arr:
+                return 0
+            
+            curr = arr[0]
+            
+            for i in range(1, len(arr)):
+                if curr == -arr[i]:
+                    return 1 + dfs(arr[1:i] + arr[i+1:])
+            
+            res = float('inf')
+            for i in range(1, len(arr)):
+                if curr*arr[i] < 0:
+                    b = arr[i]
+                    res = min(res, 1 + dfs(arr[1:i] + arr[i+1:] + [b + curr]))
+            
+            return res
+        
+        return dfs(buckets)
+        
 
-        balances = defaultdict(int)
-        for x, y, z in transactions:
-            balances[x] -= z
-            balances[y] += z
-        
-        ids = []
-        for key in balances:
-            if balances[key] != 0:
-                ids.append(balances[key])
-        N = len(ids)
-        self.count = float('inf')
-        
-        
+# method 2, O( (n-1)! )
+        hmap = defaultdict(int)
+        for u, v, w in transactions:
+            hmap[u] -= w
+            hmap[v] += w
+
+        buckets = [v for v in hmap.values() if v]
+        N = len(buckets)
+        self.res = float('inf')
+
         def dfs(start, count):
-            while start < N and ids[start] == 0:
+            while start < N and buckets[start] == 0:
                 start += 1
-            if start >= N:
-                if count < self.count:
-                    self.count = count
+                
+            if start == N:
+                if count < self.res:
+                    self.res = count
                 return
             
+            # pruning 1
+            if count >= self.res:
+                return
+            
+            curr = buckets[start]
             for i in range(start+1, N):
-                if ids[start]*ids[i] < 0:
-                    ids[i] += ids[start]
+                if curr*buckets[i] < 0:
+                    buckets[i] += curr
                     dfs(start+1, count+1)
-                    ids[i] -= ids[start]
-                    
-                    if ids[i] + ids[start] == 0:  # pruning
-                        break
-                        
-        dfs(0,0)
-        return self.count
+                    buckets[i] -= curr
+                
+                # pruning 2. If curr == -buckets[i], no need to countinue
+                if curr + buckets[i] == 0:
+                    break
+        
+        dfs(0, 0)
+
+        return self.res
