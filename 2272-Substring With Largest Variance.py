668abe05019fe2c47358ffdf0cb3782b661f215e
Chou, Shao-Chi <shao-chi.chou@fujitsu.com>
2023-07-09 18:46:25 -0500

    update Leetcode 2272, add a better solution (note that we cannot use ordinary Kadane's algo in this problem)

--- 2272-Substring With Largest Variance.py
+++ 2272-Substring With Largest Variance.py
@@ -37,3 +37,38 @@ class Solution(object):
                 res = max(res, Kadane(arr), Kadane([-x for x in arr]))
                 
         return res
+
+
+# Better solution, https://www.youtube.com/watch?v=P6KnO-Dw0Fo&ab_channel=HuifengGuan
+# Note that this problem cannot be solved using ordinary Kadane's algorithm
+
+        chars = list(set(s))
+        N = len(chars)
+        ans = 0
+        
+        def dp(arr):
+            res = 0
+            dp0, dp1 = 0, float('-inf')
+            
+            for i in range(len(arr)):
+                if arr[i] == 1:
+                    dp0 += 1
+                    dp1 += 1
+                else:
+                    dp1 = max(dp0 - 1, dp1 - 1)
+                    dp0 = 0
+                res = max(res, dp1)
+            return res
+        
+        
+        for i in range(N):
+            for j in range(i):
+                arr = []
+                for c in s:
+                    if c == chars[i]:
+                        arr.append(1)
+                    elif c == chars[j]:
+                        arr.append(-1)
+                ans = max(ans, dp(arr), dp([-x for x in arr]))
+        
+        return ans
