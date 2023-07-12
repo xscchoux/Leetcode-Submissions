f944594442b3c87a1a77ba45312e47831218d8f2
Chou, Shao-Chi <shao-chi.chou@fujitsu.com>
2023-07-12 02:00:36 -0500

    update Leetcode 802 (add topological sort solution)

--- 802-Find Eventual Safe States.py
+++ 802-Find Eventual Safe States.py
@@ -1,27 +1,55 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        
-        n = len(graph)
-        
-        # 0: unvisited, 1: safe, 2: unsafe
-        color = [0]*n
+# grey-white-black algorithm
         res = []
-
-        def dfs(curr):
-            if color[curr] == 2:
-                return False
-            elif color[curr] == 1:
+        N = len(graph)
+        color = [0]*N # 0:unvisited, 1:safe, 2:unsafe
+        
+        @cache
+        def dfs(i):
+            if color[i] == 1:
                 return True
+            elif color[i] == 2:
+                return False
             else:
-                color[curr] = 2
-                for nxt in graph[curr]:
+                if not graph[i]:
+                    color[i] = 1
+                    return True
+                color[i] = 2
+                for nxt in graph[i]:
                     if not dfs(nxt):
                         return False
-                color[curr] = 1
+                color[i] = 1
                 return True
-                     
-        for i in range(n):
+        
+        for i in range(len(graph)):
             if dfs(i):
                 res.append(i)
-                
+
         return res
+
+
+# topological sort, need to construct a graph first
+        res = []
+        N = len(graph)
+        indegree = [0]*N
+        adj = [[] for _ in range(N)]
+        
+        for i, arr in enumerate(graph):
+            for node in arr:
+                adj[node].append(i)
+                indegree[i] += 1
+                
+        queue = deque([i for i in range(N) if not indegree[i]])
+        
+        while queue:
+            for _ in range(len(queue)):
+                curr = queue.popleft()
+                res.append(curr)
+                for nxt in adj[curr]:
+                    indegree[nxt] -= 1
+                    if indegree[nxt] == 0:
+                        queue.append(nxt)
+        
+        return sorted(res)
+    
