1bc8d3931127a063c51daa8cb965bf035b04fc68
Chou, Shao-Chi <shao-chi.chou@fujitsu.com>
2023-07-24 12:18:28 -0500

    update Leetcode 50 (reattempt 3 years later)

--- 50-Pow(x, n).py
+++ 50-Pow(x, n).py
@@ -17,3 +17,21 @@ class Solution(object):
             return half*half
         else:
             return half*half*x
+
+
+        posN = True
+        if n < 0:
+            posN = False
+            n = -n
+        
+# second visitd
+        @cache
+        def dfs(n):
+            if n == 0:
+                return 1
+            if n%2 == 0:
+                return dfs(n//2)*dfs(n//2)
+            else:
+                return dfs(n//2)*dfs(n//2)*x
+            
+        return dfs(n) if posN else 1/dfs(n)
