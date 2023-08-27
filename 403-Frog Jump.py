ec84cd0024ff3bc814e51cae91ecad43ea84b08d
John Doe <john@doe.org>
2023-08-26 21:09:11 -0500

    update Leetcode 403 (add a better DFS + memoization solution)

--- 403-Frog Jump.py
+++ 403-Frog Jump.py
@@ -1,23 +1,33 @@
-from collections import defaultdict
-class Solution(object):
-    def canCross(self, stones):
-        """
-        :type stones: List[int]
-        :rtype: bool
-        """
-# Time complexity: O(n^2) (two nested loops)
-# Space complexity: O(n^2)
-
+class Solution:
+    def canCross(self, stones: List[int]) -> bool:
+        
+# Bottom-up
         hmap = defaultdict(set)
-        
-        for i in range(len(stones)):
-            hmap[stones[i]] = set()
-            
         hmap[0].add(0)
+         
+        for i in range(len(stones)-1):
+            if stones[i] in hmap:
+                for v in hmap[stones[i]]:
+                    for diff in (-1, 0, 1):
+                        if v + diff > 0:
+                            if stones[i] + v + diff == stones[-1]:
+                                return True
+                            hmap[stones[i] + v + diff].add(v + diff)
+        return False
+
+# DFS + memoization
+    
+        stoneSet = set(stones)
         
-        for i in range(len(stones)):
-            for jump in hmap[stones[i]]:
-                for diff in (-1,0,1):
-                    if jump + diff > 0:
-                        hmap[stones[i]+jump+diff].add(jump+diff)
-        return hmap[stones[-1]]
+        @cache
+        def dfs(stone, prevStep):
+            if stone == stones[-1]:
+                return True
+            
+            for diff in (prevStep-1, prevStep, prevStep+1):
+                if diff > 0 and stone+diff in stoneSet:
+                    if dfs(stone + diff, diff):
+                        return True
+            return False
+    
+        return dfs(0, 0)
