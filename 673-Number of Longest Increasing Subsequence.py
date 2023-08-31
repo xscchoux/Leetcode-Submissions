a7350b908cf8d5c43acafd625b7375bfb8adf183
John Doe <john@doe.org>
2023-08-31 03:10:12 -0500

    update Leetcode 673 (add a more concise solution)

--- 673-Number of Longest Increasing Subsequence.py
+++ 673-Number of Longest Increasing Subsequence.py
@@ -31,3 +31,28 @@ class Solution(object):
                 res += count[i]
                 
         return res
+
+
+
+# Redo. This solution is better
+        N = len(nums)
+        dp = [1]*N    # max length of the subsequence that ends at index i
+        count = [1]*N # number of subsequences with maxLength that ends at index i
+        
+        for i in range(N):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    if dp[j] + 1 > dp[i]:
+                        dp[i] = dp[j] + 1
+                        count[i] = count[j]
+                    elif dp[j] + 1 == dp[i]:
+                        count[i] += count[j]
+      
+        maxLength = max(dp)
+        res = 0
+        
+        for i in range(N):
+            if dp[i] == maxLength:
+                res += count[i]
+        
+        return res
