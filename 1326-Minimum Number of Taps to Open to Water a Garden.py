099933a10482b2e20a284d3960dab462bdbfbbd9
John Doe <john@doe.org>
2023-08-31 02:06:32 -0500

    update Leetcode 1326 (add another two solution, the greedy O(n) solution resembles Jump Game II )

--- 1326-Minimum Number of Taps to Open to Water a Garden.py
+++ 1326-Minimum Number of Taps to Open to Water a Garden.py
@@ -5,6 +5,7 @@ class Solution(object):
         :type ranges: List[int]
         :rtype: int
         """
+# O(nlogn)
         span = []
         for i in range(len(ranges)):
             span.append([max(0, i-ranges[i]), min(i+ranges[i], n)])
@@ -34,3 +35,43 @@ class Solution(object):
             maxDis = nxtMaxDis
 
         return res
+
+
+# O(n*m)
+        dp = [float('inf')]*(n+1)
+        dp[0] = 0
+        
+        for i in range(n+1):
+            left = max(0, i-ranges[i])
+            right = min(n, i+ranges[i])
+            for j in range(left, right+1):
+                dp[right] = min(dp[right], dp[j] + 1)
+        
+        if dp[n] == float('inf'):
+            return -1
+        
+        return dp[n]
+
+
+# O(n)
+        maxReach = defaultdict(int)
+        for i in range(n+1):
+            left = max(0, i-ranges[i])
+            right = min(n, i+ranges[i])
+            maxReach[left] = max(maxReach[left], right)
+        
+        currReach = 0
+        nxtReach = 0
+        tap = 0
+        
+        for i in range(n+1):
+            if i > nxtReach:
+                return -1
+            
+            if i > currReach:
+                tap += 1
+                currReach = nxtReach
+            
+            nxtReach = max(nxtReach, maxReach[i])
+        
+        return tap
