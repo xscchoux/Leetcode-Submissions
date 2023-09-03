2ba53d718022239e25e1f2c23803d9262ff17d0b
John Doe <john@doe.org>
2023-09-02 19:24:30 -0500

    Update Leetcode 2707 (add DP + Trie solution)

--- 2707-Extra Characters in a String.py
+++ 2707-Extra Characters in a String.py
@@ -29,3 +29,58 @@ class Solution:
             return res
         
         return len(s) - dfs(N-1)
+
+# bottom-up2 
+        dictionary = set(dictionary)
+        N = len(s)
+        dp = [0]*(N+1)
+        
+        for start in range(N-1, -1, -1):
+            dp[start] = 1 + dp[start+1]
+            for end in range(start, N):
+                if s[start:end+1] in dictionary:
+                    dp[start] = min(dp[end+1], dp[start])
+        
+        return dp[0]
+
+# DP + Trie
+class TrieNode:
+    def __init__(self):
+        self.children = dict()
+        self.isWord = False
+
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        N = len(s)
+        
+        def buildTrie():
+            root = TrieNode()
+            for word in dictionary:
+                node = root
+                for w in word:
+                    if w not in node.children:
+                        node.children[w] = TrieNode()
+                    node = node.children[w]
+                node.isWord = True
+            return root
+        
+        root = buildTrie()
+        
+        @cache
+        def dfs(start):
+            if start == N:
+                return 0
+            
+            res = 1 + dfs(start+1)
+            node = root
+            
+            for end in range(start, N):
+                if s[end] in node.children:
+                    node = node.children[s[end]]
+                    if node.isWord:
+                        res = min(res, dfs(end+1))
+                else:
+                    break
+            return res
+            
+        return dfs(0)
