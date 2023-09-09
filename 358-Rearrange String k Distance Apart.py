20428b76b24b281c09ff7f6e740c9c2ec9894ad5
John Doe <john@doe.org>
2023-09-09 17:15:04 -0500

    update Leetcode 358 (add a O(n) greedy solution, brilliant but tough)

--- 358-Rearrange String k Distance Apart.py
+++ 358-Rearrange String k Distance Apart.py
@@ -1,39 +1,74 @@
-from heapq import heappush, heappop
-class Solution(object):
-    def rearrangeString(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: str
-        """
-        # very annoying edge case
-        if k == 0:
+# priority queue, O((N+U)logU), N is the length of the string s, and U is the number of unique characters in the string s
+from heapq import *
+class Solution:
+    def rearrangeString(self, s: str, k: int) -> str:
+        # priority queue
+        if k <= 1:
             return s
-        
-        hmap = collections.Counter(s)
+        count = Counter(s)
         pq = []
-        
-        for key, val in hmap.items():
+
+        # O(UlogU)
+        for key, val in count.items():
             heappush(pq, (-val, key))
         
-        s_remain = len(s)
         res = []
-        
+
+        # O(NlogU)
         while pq:
-            tmp = []
-            # should not have duplicate characters in the last chunk
-            if len(pq) < k and pq[0][0] < -1:
+            if len(pq) < k and -pq[0][0] > 1:
                 return ""
             
-            n = min(k, s_remain)
-            for i in range(n):
-                num, char = heappop(pq)
-                res.append(char)
-                if num < -1:
-                    tmp.append((num+1, char))
-
-            s_remain -= n
-            for x in tmp:
-                heappush(pq, x)
-
+            tmp = []
+            for _ in range(min(k, len(pq))):
+                tmp.append(heappop(pq))
+            
+            for cnt, key in tmp:
+                res.append(key)
+                if cnt + 1 != 0:
+                    heappush(pq, (cnt+1, key))
+                
         return "".join(res)
+        
+        
+# O(n) solution, hard but brilliant, check the detailed solution in Leetcode
+class Solution:
+    def rearrangeString(self, s: str, k: int) -> str:
+        maxFreq = 0
+        hmap = defaultdict(int)
+        for c in s:
+            hmap[c] += 1
+            maxFreq = max(maxFreq, hmap[c])
+
+        mostFrequent = set()
+        secondFrequent = set()
+        
+        for key, val in hmap.items():
+            if hmap[key] == maxFreq:
+                mostFrequent.add(key)
+            elif hmap[key] == maxFreq-1:
+                secondFrequent.add(key)
+        
+        res = [[] for _ in range(maxFreq)]
+        
+        for key in mostFrequent:
+            for i in range(maxFreq):
+                res[i].append(key)
+        
+        for key in secondFrequent:
+            for i in range(maxFreq-1):
+                res[i].append(key)
+        
+        # round-robin
+        idx = 0
+        for key in hmap:
+            if key not in mostFrequent and key not in secondFrequent:
+                for _ in range(hmap[key]):
+                    res[idx].append(key)
+                    idx = (idx + 1)%(maxFreq-1)
+
+        for x in res[:-1]:
+            if len(x) < k:
+                return ""
+        
+        return "".join(map(lambda x:"".join(x), res))
