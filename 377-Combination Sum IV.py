82e6e73f243d35b5bac3ffc9b21490f018385480
John Doe <john@doe.org>
2023-09-08 21:48:49 -0500

    update Leetcode 377 (add top-down DP solution)

--- 377-Combination Sum IV.py
+++ 377-Combination Sum IV.py
@@ -1,14 +1,27 @@
-class Solution(object):
-    def combinationSum4(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        # Bottom-Up DP
         dp = [0]*(target+1)
         dp[0] = 1
+        
         for i in range(1, target+1):
             for num in nums:
                 if i - num >= 0:
                     dp[i] += dp[i-num]
+        
         return dp[-1]
+    
+        
+        # Top-Down DP
+        @cache
+        def dfs(currVal):
+            if currVal == target:
+                return 1
+            res = 0
+            for num in nums:
+                if currVal + num <= target:
+                    res += dfs(currVal+num)
+            
+            return res
+        
+        return dfs(0)
