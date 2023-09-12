d80aff4b8e07de07413983702b30c36249592114
John Doe <john@doe.org>
2023-09-12 01:05:46 -0500

    update Leetcode 688 (add top-down DP solution, notice that top-down DP is faster than bottom-up DP)

--- 688-Knight Probability in Chessboard.py
+++ 688-Knight Probability in Chessboard.py
@@ -1,35 +1,43 @@
-class Solution(object):
-    def knightProbability(self, N, K, r, c):
-        """
-        :type N: int
-        :type K: int
-        :type r: int
-        :type c: int
-        :rtype: float
-        """
+class Solution:
+    def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
 # https://www.youtube.com/watch?v=MyJvMydR2G4
 # time complexity: O(N^2)
 # space complexity: O(N^2)
+# Bottom-Up DP
         directions = [(2,1),(1,2),(-1,2),(-2,1),(1,-2),(2,-1),(-1,-2),(-2,-1)]
-        dp = [[0]*N for _ in range(N)]
-        dp[r][c] = 1
+        dp = [[0]*n for _ in range(n)]
+        dp[row][column] = 1
         
-        for k in range(K):
-            dpNext = [[0]*N for _ in range(N)]
-            for oldR in range(N):
-                for oldC in range(N):
-                    for direction in directions:
-                        newR = oldR + direction[0]
-                        newC = oldC + direction[1]
-                        if newR<0 or newC<0 or newR>=N or newC>=N:
-                            continue
-                        dpNext[newR][newC] += dp[oldR][oldC]
-            dp, dpNext = dpNext, dp
-            
-        total = 0
+        for _ in range(k):
+            tmp = [[0]*n for _ in range(n)]
+            for r in range(n):
+                for c in range(n):
+                    for dr, dc in directions:
+                        nr = r+dr
+                        nc = c+dc
+                        if 0<=nr<n and 0<=nc<n:
+                            tmp[nr][nc] += dp[r][c]/8
+            dp = tmp
         
-        for x in range(N):
-            for y in range(N):
-                total += dp[x][y]
+        res = 0
+        for r in range(n):
+            for c in range(n):
+                res += dp[r][c]
         
-        return total/float(8**K)
+        return res
+                    
+        
+# Top-Down DP
+        directions = [(2,1),(1,2),(-1,2),(-2,1),(1,-2),(2,-1),(-1,-2),(-2,-1)]
+    
+        @cache
+        def dp(r, c, move):
+            if move == 0:
+                return 1
+            res = 0
+            for dr, dc in directions:
+                if 0<=r+dr<n and 0<=c+dc<n:
+                    res += dp(r+dr, c+dc, move-1)/8
+            return res
+        
+        return dp(row, column, k)
