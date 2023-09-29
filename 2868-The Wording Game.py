89f0acf41f5ec52282795ba4827e083a8d23c105
John Doe <john@doe.org>
2023-09-29 04:01:49 -0500

    update Leetcode 2868 (just a little update)

--- 2868-The Wording Game.py
+++ 2868-The Wording Game.py
@@ -1,5 +1,6 @@
 class Solution:
     def canAliceWin(self, a: List[str], b: List[str]) -> bool:
+# https://leetcode.com/problems/the-wording-game/discuss/4097898/Python-3-Only-lexicographically-largest-for-each-initial-letter
         maxString = [dict(), dict()]
         for c in a:
             maxString[0][c[0]] = c        
@@ -7,13 +8,13 @@ class Solution:
             maxString[1][c[0]] = c
         
         @cache
-        def dp(currFirst, nxt):   # A:0, B:1
-            if currFirst in maxString[nxt^1] and maxString[nxt][currFirst] < maxString[nxt^1][currFirst]:
-                if not dp(currFirst, nxt^1):
+        def dp(currFirst, currIdx):   # A:0, B:1
+            if currFirst in maxString[currIdx^1] and maxString[currIdx][currFirst] < maxString[currIdx^1][currFirst]:
+                if not dp(currFirst, currIdx^1):
                     return True
             
-            if ord(currFirst) + 1 <= ord('z') and chr(ord(currFirst) + 1) in maxString[nxt^1]:
-                if not dp(chr(ord(currFirst) + 1), nxt^1):
+            if ord(currFirst) + 1 <= ord('z') and chr(ord(currFirst) + 1) in maxString[currIdx^1]:
+                if not dp(chr(ord(currFirst) + 1), currIdx^1):
                     return True
             
             return False # nxt cannot play on their turn
