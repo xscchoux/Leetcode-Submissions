bd6140666e2052bd08ee4b48ae91fe4ed672120a
John Doe <john@doe.org>
2023-10-05 22:27:49 -0500

    update Leetcode 343 (add a top-down DP solution)

--- 343-Integer Break.py
+++ 343-Integer Break.py
@@ -12,3 +12,20 @@ class Solution(object):
             return 3*breaker(n-3)
         
         return breaker(n)
+
+
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        if n == 2 or n == 3:
+            return n - 1
+        
+        @cache
+        def dfs(val):
+            if val <= 3:
+                return val
+            res = val
+            for i in range(2, val):
+                res = max(res, i*dfs(val-i))
+            return res
+            
+        return dfs(n)
