2c1e749bde560dda58ce93cb41d2c3754303234b
John Doe <john@doe.org>
2023-11-27 04:28:50 -0600

    update Leetcode 1887 (use reverse iterator)

--- C++/1887-Reduction Operations to Make the Array Elements Equal.cpp
+++ C++/1887-Reduction Operations to Make the Array Elements Equal.cpp
@@ -13,3 +13,22 @@ public:
         return res;
     }
 };
+
+
+// use reverse iterator:
+
+class Solution {
+public:
+    int reductionOperations(vector<int>& nums) {
+        map<int, int> hmap;
+        for (int num:nums){
+            hmap[num]++;
+        }
+        int res = 0, count = 0;
+        for (auto it = hmap.rbegin(); it != prev(hmap.rend()); ++it) {
+            count += it->second;
+            res += count;
+        }        
+        return res;
+    }
+};
