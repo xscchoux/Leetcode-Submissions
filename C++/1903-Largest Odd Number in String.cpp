5e084ab000d865951d249480c054b8f5c21b292b
John Doe <john@doe.org>
2023-12-07 01:59:16 -0600

    add Leetcode 1903 (C++, remember how to convert char to number)

---
+++ C++/1903-Largest Odd Number in String.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        int i = num.size() - 1;
+        while (i >= 0) {
+            if ((num[i] - '0')%2 > 0) return num.substr(0, i+1);
+            i--;
+        }
+
+        return "";
+    }
+};
