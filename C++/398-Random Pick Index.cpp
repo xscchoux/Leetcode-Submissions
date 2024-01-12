1a34fa7742406f6590f639984b2f6f8a28f2388f
John Doe <john@doe.org>
2024-01-12 02:21:07 -0600

    add Leetcode 398 (C++, use rand(), reservoir sampling solution gives TLE)

---
+++ C++/398-Random Pick Index.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    unordered_map<int, vector<int>> hmap;
+    Solution(vector<int>& nums) {
+        for (int i=0; i<nums.size(); i++) {
+            hmap[nums[i]].push_back(i);
+        }
+    }
+    
+    int pick(int target) {
+        int size = hmap[target].size();
+        return hmap[target][rand()%size];
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * int param_1 = obj->pick(target);
+ */
