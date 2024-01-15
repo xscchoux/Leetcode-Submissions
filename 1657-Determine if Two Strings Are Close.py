c8717b71656edf5ce093dde847a4b6c0871ce809
John Doe <john@doe.org>
2024-01-14 18:44:07 -0600

    add Leetcode 1657 (the most concise solution)

---
+++ 1657-Determine if Two Strings Are Close.py
@@ -0,0 +1,5 @@
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        c1, c2 = Counter(word1), Counter(word2)
+        
+        return sorted(c1.keys()) == sorted(c2.keys()) and sorted(c1.values()) == sorted(c2.values())
