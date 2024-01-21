697ce3137a4a1fc4842f39f9679ac02ce5cd1588
John Doe <john@doe.org>
2024-01-21 01:40:59 -0600

    add Leetcode 931 (C++, easy)

---
+++ C++/931-Minimum Falling Path Sum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        for (int i=1; i<matrix.size(); i++){
+            for (int j=0; j<matrix[0].size(); j++){
+                if (j == 0) matrix[i][j] = matrix[i][j] += min(matrix[i-1][j], matrix[i-1][j+1]);
+                else if (j == matrix[0].size()-1) matrix[i][j] += min(matrix[i-1][j], matrix[i-1][j-1]);
+                else  matrix[i][j] += min(matrix[i-1][j-1],  min(matrix[i-1][j], matrix[i-1][j+1]));
+            }       
+        }
+        return *min_element(matrix[matrix.size()-1].begin(), matrix[matrix.size()-1].end());
+    }
+};
