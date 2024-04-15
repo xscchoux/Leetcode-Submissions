cf9ae46abe4a969b147ce33e7e3964b9fd638d5f
John Doe <ed78617@gmail.com>
2024-04-15 13:49:51 -0500

    add Leetcode 2073 (C++, boring)

---
+++ C++/2073-Time Needed to Buy Tickets.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int res = 0;
+        
+        for (int i=0; i<tickets.size(); i++) {
+            if (i<=k) {
+                res += min(tickets[k], tickets[i]);
+            } else {
+                res += min(tickets[k]-1, tickets[i]);
+            }
+        }
+        
+        return res;
+    }
+};
