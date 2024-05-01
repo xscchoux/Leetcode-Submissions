6bd49ba4c7d62a7bd9b52722001c97ae2774d232
John Doe <ed78617@gmail.com>
2024-05-01 12:44:49 -0500

    add Leetcode 2000 (C++, practice reversing a substring using iterator)

---
+++ C++/2000-Reverse Prefix of Word.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int ind = word.find(ch);
+        
+        if (ind == -1) return word;
+
+        reverse(word.begin(), word.begin() + ind + 1 );
+
+        return word;
+    }
+};
