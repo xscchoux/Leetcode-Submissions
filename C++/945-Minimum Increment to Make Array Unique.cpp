dd27dcc3bf9fd393dbeb65e44b3de0ab63b83aeb
John Doe <ed78617@gmail.com>
2024-06-14 05:35:32 -0500

    add Leetcode 945 (C++, easy one)

---
+++ C++/945-Minimum Increment to Make Array Unique.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int res = 0;
+
+        for (int i=1; i<nums.size(); i++) {
+            if (nums[i] <= nums[i-1]) {
+                res += nums[i-1] - nums[i] + 1;
+                nums[i] = nums[i-1] + 1;
+            }
+        }
+
+        return res;
+    }
+};
