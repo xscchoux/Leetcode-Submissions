d1e51aafc886ac6d1a62151b7462d5e8f5164ea5
John Doe <ed78617@gmail.com>
2024-09-26 12:10:17 -0500

    update Leetcode 440 (C++, add digit prefix count solution, tough)

--- C++/440-K-th Smallest in Lexicographical Order.cpp
+++ C++/440-K-th Smallest in Lexicographical Order.cpp
@@ -23,3 +23,44 @@ public:
         return res;
     }
 };
+
+
+// digit prefix count
+// https://github.com/wisdompeak/LeetCode/tree/master/Others/440.K-th-Smallest-in-Lexicographical-Order
+using LL = long long;
+class Solution {
+public:
+    int countSuffix(int prefix, int n) {
+        int count = 0;
+        LL exp = 10;
+        while (true) {
+            LL lower = prefix*exp, upper = prefix*exp + exp - 1;
+            if (lower > n) break;
+            else if (n >= lower && n <= upper) {
+                count += n-lower+1;
+            } else {
+                count += exp;
+            }
+            exp *= 10;
+        }
+
+        return count;
+    }
+    int findKthWithPrefix(int prefix, int n, int k) {
+        if (k > 0) {
+            int start = (prefix==0)?1:0;
+            for (int i=start; i<=9; i++) {
+                int count = 1 + countSuffix(prefix*10+i, n);
+                if (count < k) {
+                    k -= count;
+                } else {
+                    return findKthWithPrefix(prefix*10+i, n, k-1);
+                }
+            }
+        }
+        return prefix; // when k == 0
+    }
+    int findKthNumber(int n, int k) {
+        return findKthWithPrefix(0, n, k);
+    }
+};
