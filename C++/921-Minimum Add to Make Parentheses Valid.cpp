64fecf4bb68c6aebb8fe3ae608a552f5d8a82eef
John Doe <ed78617@gmail.com>
2024-10-08 23:32:48 -0500

    add Leetcode 921 (C++, greedy, easy)

---
+++ C++/921-Minimum Add to Make Parentheses Valid.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int open = 0, close = 0;
+        for (char c:s) {
+            if (c == '(') {
+                open++;
+            } else {
+                if (open > 0) open--;
+                else close++;
+            }
+        }
+
+        return open + close;
+    }
+};
