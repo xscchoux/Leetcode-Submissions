da5c4b82824bd1d8b70a5e5d7cfc8c6ad7e2764a
John Doe <ed78617@gmail.com>
2024-11-10 11:01:33 -0600

    update Leetcode 678 (medium, add a clever constant space solution)

--- 678-Valid Parenthesis String.py
+++ 678-Valid Parenthesis String.py
@@ -24,3 +24,51 @@ class Solution(object):
                 min_op = 0
         
         return min_op == 0
+
+
+# Use two stacks
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        left = []
+        star = []
+        for i, c in enumerate(s):
+            if c == "(":
+                left.append(i)
+            elif c == "*":
+                star.append(i)
+            else:
+                if left:
+                    left.pop()
+                elif star:
+                    star.pop()
+                else:
+                    return False
+        
+        while left:
+            if star and left.pop() < star.pop():
+                continue
+            return False
+        
+        return True
+
+
+# costant space
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        openParen = closeParen = 0 # maximum number of left parantheses and right parantheses 
+        N = len(s)
+        for i in range(N):
+            if s[i] in ("(", "*"):
+                openParen += 1
+            else:
+                openParen -= 1
+            
+            if s[N-i-1] in (")", "*"):
+                closeParen += 1
+            else:
+                closeParen -= 1
+            
+            if openParen < 0 or closeParen < 0:
+                return False
+        
+        return True
