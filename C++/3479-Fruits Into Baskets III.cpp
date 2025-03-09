d73c878f2c3049d6e4db17527e14d29bcf274ac4
John Doe <ed78617@gmail.com>
2025-03-09 10:44:01 -0500

    add Leetcode 3479 (C++, hard, binary search + segment tree, find maximum in a certain range)

---
+++ C++/3479-Fruits Into Baskets III.cpp
@@ -0,0 +1,91 @@
+// O(N*logN*logN)
+class SGTree {
+	vector<int> seg;
+public:
+	SGTree(int n) {
+		seg.resize(4 * n + 1, 0);
+	}
+
+	void build(int ind, int low, int high, vector<int>& arr) {
+		if (low == high) {
+			seg[ind] = arr[low];
+			return;
+		}
+
+		int mid = (low + high) / 2;
+		build(2 * ind + 1, low, mid, arr);
+		build(2 * ind + 2, mid + 1, high, arr);
+		seg[ind] = max(seg[2 * ind + 1], seg[2 * ind + 2]);
+	}
+
+	int query(int ind, int low, int high, int l, int r) {
+		// no overlap
+		// l r low high or low high l r
+		if (r < low || high < l) return 0;
+
+		// complete overlap
+		// [l low high r]
+		if (low >= l && high <= r) return seg[ind];
+
+		int mid = (low + high) >> 1;
+		int left = query(2 * ind + 1, low, mid, l, r);
+		int right = query(2 * ind + 2, mid + 1, high, l, r);
+		return max(left, right);
+	}
+
+	void update(int ind, int low, int high, int i, int val) {
+		if (low == high) {
+			seg[ind] = val;
+			return;
+		}
+
+		int mid = (low + high) >> 1;
+		if (i <= mid) update(2 * ind + 1, low, mid, i, val);
+		else update(2 * ind + 2, mid + 1, high, i, val);
+		seg[ind] = max(seg[2 * ind + 1], seg[2 * ind + 2]);
+	}
+};
+
+class Solution {
+public:
+    int N;
+    int find(int curr, SGTree& sgt) {
+        int left = 0, right = N-1, res = 0;
+        while (left + 1 < right) {
+            int mid = left + (right-left)/2;
+            int maxVal = sgt.query(0, 0, N-1, 0, mid);  // finding max value in the range 0 to mid
+            if (maxVal >= curr) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+
+        if (sgt.query(0, 0, N-1, 0, right) < curr) {
+            return 1;
+        }
+
+        int idxChanged;
+        if (sgt.query(0, 0, N-1, 0, left) >= curr) {
+            idxChanged = left;
+        } else {
+            idxChanged = right;
+        }
+
+        sgt.update(0, 0, N-1, idxChanged, 0);
+        return 0;
+    }
+
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        this->N = baskets.size();
+        SGTree sgt(N);
+        sgt.build(0, 0, N-1, baskets);
+        int res = 0;
+
+        for (int i=0; i<N; i++) {
+            res += find(fruits[i], sgt);
+        }
+
+        return res;
+    }
+};
