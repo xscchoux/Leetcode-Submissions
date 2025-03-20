90d30cca5c31fe76cb393d44fc085009c61dd237
John Doe <ed78617@gmail.com>
2025-03-20 14:01:02 -0500

    update Leetcode 3108 (C++, medium-hard, added a faster solution, replaced unordered_map with vector and reduced loop iterations)

--- C++/3108-Minimum Cost Walk in Weighted Graph.cpp
+++ C++/3108-Minimum Cost Walk in Weighted Graph.cpp
@@ -48,3 +48,63 @@ public:
         return res;
     }
 };
+
+
+
+// Redo, this is much faster
+struct UnionFind {
+    vector<int> parent, rank;
+    explicit UnionFind(int n) : parent(vector<int>(n)), rank(vector<int>(n, 1)) {
+        iota(parent.begin(), parent.end(), 0);
+    }
+    int find(int node) {
+        if(node == parent[node]) return node;
+        return parent[node] = find(parent[node]);
+    }
+    bool unite(int node1, int node2) {
+        int p1 = find(node1);
+        int p2 = find(node2);
+        if(p1 == p2) return false; // Same parent
+        if(rank[p1] >= rank[p2]) {
+            // Append p2 under p1
+            parent[p2] = p1;
+            rank[p1] += rank[p2];
+        }else {
+            parent[p1] = p2;
+            rank[p2] += rank[p1];
+        }
+        return true;
+    }
+};
+class Solution {
+public:
+    vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
+        UnionFind uf(n);
+
+        for (auto &e:edges) {
+            int u = e[0], v = e[1], w = e[2];
+            uf.unite(u, v);
+        }
+
+        vector<int> res;
+        vector<int> val(n, -1);  // note this trick, we use -1
+
+        // calculate AND values of all connected components
+        for (auto &e:edges) {
+            int root = uf.find(e[0]);
+            val[root] &= e[2];
+        }
+
+
+        for (auto &q:query) {
+            int u = q[0], v = q[1];
+            if (uf.find(u) != uf.find(v)) res.push_back(-1);
+            else {
+                int root = uf.find(u);
+                res.push_back(val[root]);
+            }
+        }
+
+        return res;
+    }
+};
