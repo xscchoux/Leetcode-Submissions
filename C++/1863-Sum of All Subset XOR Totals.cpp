99eccefca146780f110ac86fa6f6e2fbee011fe2
John Doe <ed78617@gmail.com>
2025-04-05 13:02:29 -0500

    update Leetcode 1863 (C++, medium, add very clever O(n) solution)

--- C++/1863-Sum of All Subset XOR Totals.cpp
+++ C++/1863-Sum of All Subset XOR Totals.cpp
@@ -19,3 +19,37 @@ public:
         return res;
     }
 };
+
+
+// O(N)
+// Each '1' has 2^(N-1) combinations:
+// If we pick other even number of '1', use this '1'
+// If we pick other odd number of '1', don't use this '1'
+
+// Detail: https://leetcode.cn/problems/sum-of-all-subset-xor-totals/solutions/3614974/on-shu-xue-zuo-fa-pythonjavaccgojsrust-b-9txy/
+
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        int tot = 0;
+        for (int num:nums) tot |= num;
+
+        return tot << (nums.size()-1);
+
+    }
+};
+
+// Use ranges::fold_left()
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        return ranges::fold_left(nums, 0, [](int a, int b) { return a|b; }) << (nums.size()-1);
+    }
+};
+
+class Solution {
+public:
+    int subsetXORSum(vector<int>& nums) {
+        return ranges::fold_left(nums, 0, bit_or{}) << (nums.size()-1);
+    }
+};
