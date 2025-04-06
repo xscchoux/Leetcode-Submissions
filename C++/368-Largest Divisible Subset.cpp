453c815fc4a00736dec03c91959603ed3dde1908
John Doe <ed78617@gmail.com>
2025-04-06 03:41:57 -0500

    update Leetcode 368 (C++, medium, dp, add a solution that beats 99%)

--- C++/368-Largest Divisible Subset.cpp
+++ C++/368-Largest Divisible Subset.cpp
@@ -40,3 +40,42 @@ public:
         return res;
     }
 };
+
+
+
+
+// Second visit, beat 99%
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        sort(begin(nums), end(nums));
+        int N = nums.size(), maxLen = 1, maxNum = nums[0];
+        vector<int> dp(N, 1);
+
+
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<i; j++) {
+                if (nums[i]%nums[j] == 0) {
+                    dp[i] = max(dp[i], dp[j]+1);
+                    if (dp[i] > maxLen) {
+                        maxLen = dp[i];
+                        maxNum = nums[i];
+                    }
+                }
+            }
+        }
+
+        vector<int> res;
+        for (int i=N-1; i>=0; i--) {
+            if (dp[i] == maxLen && maxNum%nums[i] == 0) {
+                res.push_back(nums[i]);
+                maxNum = nums[i];
+                maxLen--;
+            }
+        }
+
+        reverse(begin(res), end(res));
+
+        return res;
+    }
+};
