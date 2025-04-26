6df67209ca50df0b2a34dacab9c2f06b934c59a5
John Doe <ed78617@gmail.com>
2025-04-26 04:44:10 -0500

    update Leetcode 2444 (C++, medium-hard, use 'if with initializer' syntax)

--- C++/2444-Count Subarrays With Fixed Bounds.cpp
+++ C++/2444-Count Subarrays With Fixed Bounds.cpp
@@ -17,3 +17,33 @@ public:
         return res;
     }
 };
+
+
+// Second visit
+// This uses a proper C++17 "if with initializer" syntax
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int minK, int maxK) {
+        int N = nums.size(), minPos = -1, maxPos = -1, leftBound = -1;
+        long long res = 0;
+
+        for (int i=0; i<N; i++) {
+            if (nums[i] < minK || nums[i] > maxK) {
+                leftBound = i;
+            }
+
+            if (nums[i] == minK) {
+                minPos = i;
+            }
+            if (nums[i] == maxK) {
+                maxPos = i;
+            }
+
+            if (int x = min(minPos, maxPos)-leftBound; x > 0) {
+                res += x;
+            }
+        }
+
+        return res;
+    }
+};
