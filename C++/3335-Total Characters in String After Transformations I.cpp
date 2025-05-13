37e6304bce4d3ab342b85eda28e0d64e59570f8e
John Doe <ed78617@gmail.com>
2025-05-13 06:02:32 -0500

    update Leetcode 3335 (C++, medium, added a much faster solution with same logic)

--- C++/3335-Total Characters in String After Transformations I.cpp
+++ C++/3335-Total Characters in String After Transformations I.cpp
@@ -24,3 +24,30 @@ public:
         return res;
     }
 };
+
+
+// Same logic but much faster
+using LL = long long;
+class Solution {
+public:
+    int kMod = 1e9+7;
+    int lengthAfterTransformations(string s, int t) {
+        int cnt[26] = {0}, res = s.size();
+        for (char c:s) cnt[c-'a']++;
+
+        while (t > 0) {
+            int tmp = cnt[0];
+            for (int i=1; i<=25; i++) {
+                int tmp2 = cnt[i];
+                cnt[i] = tmp;
+                tmp = tmp2;
+            }
+            cnt[0] = tmp;
+            cnt[1] = (cnt[1] + tmp)%kMod;
+            res = (res + tmp)%kMod;
+            t--;
+        }
+
+        return res;
+    }
+};
