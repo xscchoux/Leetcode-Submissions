3eb74b3734e95fe1e6e11102444887dfc61322c9
John Doe <ed78617@gmail.com>
2025-05-25 13:46:23 -0500

    update Leetcode 3068 (C++, hard, added a faster solution with one trick, tricky XOR math)

--- C++/3068-Find the Maximum Sum of Node Values.cpp
+++ C++/3068-Find the Maximum Sum of Node Values.cpp
@@ -15,3 +15,27 @@ public:
         return res - (update%2?diff:0);
     }
 };
+
+
+
+// Faster solution with one trick 
+class Solution {
+public:
+    long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
+        // we can only change even number of nodes
+        long long res = 0, minDiff = LLONG_MAX;
+        int isOdd = 0;
+        for (int i=0; i<nums.size(); i++) {
+            int newNum = nums[i]^k;
+            res += max(nums[i], newNum);
+            if (newNum > nums[i]) {
+                isOdd = 1 - isOdd;
+            }
+            minDiff = min(minDiff, (long long)abs(newNum - nums[i]));
+            // if newNum - nums[i] is negative, and isOdd is 1, we change one node
+            // if newNum - nums[i] is positive, and isOdd is 1, we revert the change of one node
+        }
+
+        return res - isOdd*minDiff;
+    }
+};
