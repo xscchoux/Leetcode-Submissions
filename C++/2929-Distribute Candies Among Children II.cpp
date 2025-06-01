0fab84b961958b71d7cf119eacd098bf4124c24b
John Doe <ed78617@gmail.com>
2025-06-01 12:36:48 -0500

    update Leetcode 2929 (C++, medium-hard, added a solution using inclusion-exclusion principle)

--- C++/2929-Distribute Candies Among Children II.cpp
+++ C++/2929-Distribute Candies Among Children II.cpp
@@ -13,3 +13,17 @@ public:
         return res;
     }
 };
+
+
+// inclusion-exclusion principle, O(1)
+class Solution {
+public:
+    long long H(int n) {  // H(3, n) = C(n+2, n) = C(n+2, 2)
+        if (n < 0) return 0;
+
+        return (long long)(n+2)*(n+1)/2;
+    }
+    long long distributeCandies(int n, int limit) {
+        return H(n) - 3*H(n-(limit+1)) + 3*H(n-2*(limit+1)) - H(n-3*(limit+1));
+    }
+};
