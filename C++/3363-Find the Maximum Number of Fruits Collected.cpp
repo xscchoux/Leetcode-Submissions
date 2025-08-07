561944868f17c1d0f221066465c25a063b7b6995
John Doe <ed78617@gmail.com>
2025-08-07 10:53:40 -0500

    update Leetcode 3363 (C++, hard, DP, add a more concise solution with same time complexity, good problem)

--- C++/3363-Find the Maximum Number of Fruits Collected.cpp
+++ C++/3363-Find the Maximum Number of Fruits Collected.cpp
@@ -58,3 +58,46 @@ public:
         return tot;
     }
 };
+
+
+
+// Same time complexity but more concise
+// https://leetcode.cn/problems/find-the-maximum-number-of-fruits-collected/solutions/2998667/nao-jin-ji-zhuan-wan-wang-ge-tu-dppython-gjcm/
+class Solution {
+public:
+    int N;
+    int dfs(int r, int c, vector<vector<int>>& fruits, vector<vector<int>>& memo) {
+        if (c < N-1-r || c >= N) {
+            return INT_MIN;
+        }
+        if (r == 0) return fruits[r][c];  // when r == 0 and c == N-1
+
+        if (memo[r][c] != -1) return memo[r][c];
+
+        int res = fruits[r][c] + max({dfs(r-1, c, fruits, memo), dfs(r-1, c-1, fruits, memo), dfs(r-1, c+1, fruits, memo)});
+
+        return memo[r][c] = res;
+    }
+    int maxCollectedFruits(vector<vector<int>>& fruits) {
+        this->N = fruits.size();
+        int res = 0;
+
+        for (int i=0; i<N; i++) {
+            res += fruits[i][i];
+        }
+
+        vector<vector<int>> memo(N, vector<int>(N, -1));
+
+        res += dfs(N-2, N-1, fruits, memo);
+        for (int i=0; i<N; i++) {
+            for (int j=i+1; j<N; j++) {
+                fruits[i][j] = fruits[j][i];
+            }
+        }
+
+        ranges::fill(memo, vector(N, -1));
+        res += dfs(N-2, N-1, fruits, memo);
+
+        return res;
+    }
+};
