b8dfcd5857ed2eb8aed8484cc4af84cc8e492559
John Doe <ed78617@gmail.com>
2025-08-15 11:17:55 -0500

    update Leetcode 342 (C++, easy-medium, bitwise operation, added another solution using hexadecimal mask, note an ideal one, good problem)

--- C++/342-Power of Four.cpp
+++ C++/342-Power of Four.cpp
@@ -1,3 +1,14 @@
+// This is fast, but not ideal, because 0xaaaaaaaa gets implicitly promoted to unsigned int during the bitwise operation
+// 0xaa -> 10101010
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if (n <= 0) return false;
+        return (n&(n-1)) == 0 && (n&0xaaaaaaaa) == 0;  // Though 0xaaaaaaaa is greater than 2^31-1, it's converted to unsigned int here.
+    }
+};
+
+// Better
 class Solution {
 public:
     bool isPowerOfFour(int n) {
