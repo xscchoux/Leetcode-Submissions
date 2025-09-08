09bfe01db198b4c8513eafe27d72525dde2a10d9
John Doe <ed78617@gmail.com>
2025-09-08 05:57:04 -0500

    add Leetcode 1317 (C++, easy, string)

---
+++ C++/1317-Convert Integer to the Sum of Two No-Zero Integers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> getNoZeroIntegers(int n) {
+        for (int a=1; a<=n/2; a++) {
+            int b = n-a;
+            if ((to_string(a) + to_string(b)).find('0') == string::npos) {
+                return {a, b};
+            }
+        }
+
+        return {};
+    }
+};
