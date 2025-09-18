ee409f0dc0d27500c5eef2a4831df65a7bd36c41
John Doe <ed78617@gmail.com>
2025-09-18 08:14:45 -0500

    update Leetcode 3408 (C++, medium-hard, add efficient priority queue solution, good problem)

--- C++/3408-Design Task Manager.cpp
+++ C++/3408-Design Task Manager.cpp
@@ -47,3 +47,56 @@ public:
         return userId;
     }
 };
+
+
+// use hash map + priority_queue
+using PII = pair<int, int>;
+class TaskManager {
+private:
+    unordered_map<int, PII> taskHmap; // taskId: {priority, userId}
+    priority_queue<PII> pq;           // {priority, taskId}
+public:
+    TaskManager(vector<vector<int>>& tasks) {
+        for (auto &task:tasks) {
+            int userId = task[0], taskId = task[1], priority = task[2];
+            taskHmap[taskId] = make_pair(priority, userId);
+            pq.push({priority, taskId});
+        }
+    }
+
+    void add(int userId, int taskId, int priority) {
+        taskHmap[taskId] = {priority, userId};
+        pq.push({priority, taskId});
+    }
+
+    void edit(int taskId, int newPriority) {
+        taskHmap[taskId].first = newPriority;
+        pq.push({newPriority, taskId});
+    }
+    
+    void rmv(int taskId) {
+        taskHmap.erase(taskId);
+    }
+    
+    int execTop() {
+        while (!pq.empty()) {
+            auto [priority, taskId] = pq.top();
+            pq.pop();
+            if (taskHmap.contains(taskId) && priority == taskHmap[taskId].first) {
+                int userId = taskHmap[taskId].second;
+                taskHmap.erase(taskId);
+                return userId;
+            }
+        }
+        return -1;
+    }
+};
+
+/**
+ * Your TaskManager object will be instantiated and called as such:
+ * TaskManager* obj = new TaskManager(tasks);
+ * obj->add(userId,taskId,priority);
+ * obj->edit(taskId,newPriority);
+ * obj->rmv(taskId);
+ * int param_4 = obj->execTop();
+ */
