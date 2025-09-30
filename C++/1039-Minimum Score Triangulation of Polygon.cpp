c749bb4224e6f082522d4671eadfc9b0279e5dce
John Doe <ed78617@gmail.com>
2025-09-30 13:57:20 -0500

    update Leetcode 1039 (C++, hard, add a bottom-up DP solution, good problem)

--- C++/1039-Minimum Score Triangulation of Polygon.cpp
+++ C++/1039-Minimum Score Triangulation of Polygon.cpp
@@ -1,18 +1,46 @@
 class Solution {
 public:
-    int dfs(int i, int j, vector<vector<int>>& dp, vector<int>& values) {
-        if (j - i <= 1) return 0;
+    int dfs(int first, int end, vector<int>& values, vector<vector<int>>& memo) {
+        if (first + 1 == end) return 0;
+
+        if (memo[first][end] != -1) return memo[first][end];
+
         int res = INT_MAX;
-        for (int k=i+1; k<j; k++) {
-            res = min(res, (dp[i][k] == -1?dfs(i, k, dp, values):dp[i][k]) + values[i]*values[k]*values[j] + (dp[k][j] == -1?dfs(k, j, dp, values):dp[k][j]));
+        for (int k=first+1; k<end; k++) {
+            res = min(res, values[first]*values[k]*values[end] + dfs(first, k, values, memo) + dfs(k, end, values, memo));
         }
-        return dp[i][j] = res;
+
+        return memo[first][end] = res;
     }
-    
     int minScoreTriangulation(vector<int>& values) {
         int N = values.size();
-        vector<vector<int>> dp(N, vector<int>(N, -1));
-        
-        return dfs(0, N-1, dp, values);
+        vector<vector<int>> memo(N, vector<int>(N, -1));
+
+        return dfs(0, N-1, values, memo);
+
+    }
+};
+
+
+// Bottom-up DP, calculate smaller shapes first, clever but harder to understand
+class Solution {
+public:
+    int minScoreTriangulation(vector<int>& values) {
+        int N = values.size();
+        vector<vector<int>> dp(N, vector<int>(N, 0));
+
+        for (int d=2; d<N; d++) {
+            for (int start=0; start<N-d; start++) {
+                int end = (start+d); // maximum value is N-1
+                int weight = values[start]*values[end];
+                int res = INT_MAX;
+                for (int k=start+1; k<start+d; k++) {
+                    res = min(res, weight*values[k] + dp[start][k] + dp[k][end]);
+                }
+                dp[start][end] = res;
+            }
+        }
+
+        return dp[0][N-1];
     }
 };
