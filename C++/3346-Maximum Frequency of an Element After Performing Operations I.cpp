b2e95356b4c7a2ff9d938ac74a67c2423e360b15
John Doe <ed78617@gmail.com>
2025-10-21 10:38:38 -0500

    update Leetcode 3346 (C++, medium-hard, added a O(max value in nums) solution using difference array, good problem)

--- C++/3346-Maximum Frequency of an Element After Performing Operations I.cpp
+++ C++/3346-Maximum Frequency of an Element After Performing Operations I.cpp
@@ -3,10 +3,8 @@ public:
     int maxFrequency(vector<int>& nums, int k, int numOperations) {
         sort(begin(nums), end(nums));
         unordered_map<int, int> cnt;
-        int maxVal = 0;
         for (int num:nums) {
             cnt[num]++;
-            maxVal = max(maxVal, num);
         }
 
         int res = 0;
@@ -16,6 +14,35 @@ public:
             res = max(res, cnt[i] + min(numOperations, rightIdx-leftIdx-cnt[i]));
         }
 
+        return res;
+    }
+};
+
+
+// clever O(max value in nums) solution without using binary search, kinda hard
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        const int N = nums.size();
+        int mn = INT_MAX, mx = *max_element(begin(nums), end(nums));
+        const int sz = mx+2;
+        vector<int> cnt(sz, 0), freq(sz, 0);
+
+        for (int num:nums) {
+            freq[num]++;
+            int leftBound = max(1, num-k);
+            int rightBound = min(num+k+1, mx+1);
+            cnt[leftBound]++;
+            cnt[rightBound]--;
+            mn = min(mn, leftBound);
+        }
+
+        int curr = 0, res = 0;
+        for (int num=mn; num<=mx; num++) {
+            curr += cnt[num];
+            res = max(res, freq[num] + min(numOperations, curr-freq[num]) );
+        }
+        
         return res;
     }
 };
