332a559838dfd675a7dd3eac9f10c72d7da17fba
John Doe <ed78617@gmail.com>
2025-11-19 08:30:27 -0600

    update Leetcode 3234 (C++, extremely hard, added another sliding window solution, good problem)

--- C++/3234-Count the Number of Substrings With Dominant Ones.cpp
+++ C++/3234-Count the Number of Substrings With Dominant Ones.cpp
@@ -23,3 +23,56 @@ public:
         return res;
     }
 };
+
+
+
+// Redo, still a tough one
+// https://github.com/wisdompeak/LeetCode/blob/master/Two_Pointers/3234.Count-the-Number-of-Substrings-With-Dominant-Ones/3234.Count-the-Number-of-Substrings-With-Dominant-Ones.cpp
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int N = s.size(), sqrtN = sqrt(N), res = 0;
+
+        vector<int> rightOnes(N, 0);  // consecutive ones starting from i+1
+        int curr = 0;
+        for (int i=N-1; i>=0; i--) {
+            rightOnes[i] = curr;
+            if (s[i] == '1') {
+                curr++;
+            } else {
+                curr = 0;
+            }
+        }
+
+        for (int m=1; m<=sqrtN; m++) {   // m: number of zeros in the window
+            int zeroCnt = 0, rightIdx = 0;
+            for (int leftIdx=0; leftIdx < N; leftIdx++) {
+                while (rightIdx < N && zeroCnt < m) {
+                    zeroCnt += (s[rightIdx] == '0');
+                    rightIdx++;
+                }
+                if (zeroCnt < m) break;
+
+                // count substrings starting with s[leftIdx] and also having m zeros and dominant ones
+                int oneCnt = rightIdx-leftIdx - m;
+                if (m*m <= oneCnt + rightOnes[rightIdx-1]) {
+                    int extraOnes = rightOnes[rightIdx-1] - max(0, m*m-oneCnt);
+                    res += extraOnes+1;
+                }
+                zeroCnt -= (s[leftIdx] == '0');
+            }
+        }
+
+        // substrings that only contain 1
+        for (int i=0, cnt=0; i<N; i++) {
+            if (s[i] == '1') {
+                cnt++;
+                res += cnt;
+            } else {
+                cnt = 0;
+            }
+        }
+
+        return res;
+    }
+};
