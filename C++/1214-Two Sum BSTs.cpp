626fd53f56224d0c3b1a32be9f9c34f0f05f6d06
John Doe <ed78617@gmail.com>
2025-12-01 11:33:06 -0600

    update Leetcode 1214 (C++, medium-hard if using iterative approach, redo, good problem)

--- C++/1214-Two Sum BSTs.cpp
+++ C++/1214-Two Sum BSTs.cpp
@@ -42,3 +42,61 @@ public:
         }
     }
 };
+
+
+// Redo
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool twoSumBSTs(TreeNode* root1, TreeNode* root2, int target) {
+        stack<TreeNode*> st1, st2;
+        while (root2 != nullptr) {
+            st2.push(root2);
+            root2 = root2->right;
+        }
+
+        while (true) {
+            if (root1 != nullptr) {
+                while (root1 != nullptr) {
+                    st1.push(root1);
+                    root1 = root1->left;
+                }
+            } else if (!st1.empty() && !st2.empty()) {
+                root1 = st1.top();
+                st1.pop();
+
+                while (root1->val + st2.top()->val >= target) {
+                    root2 = st2.top();
+                    st2.pop();
+                    if (root1->val + root2->val == target) return true;
+                    else if (root2->left != nullptr) {
+                        root2 = root2->left;
+                        while (root2 != nullptr) {
+                            st2.push(root2);
+                            root2 = root2->right;
+                        }
+                    } else if (st2.empty()) {
+                        break;
+                    }
+                }
+
+                root1 = root1->right;
+            } else {
+                break;
+            }
+        }
+
+        return false;
+    }
+};
