f2636ff47f9333805fb897cffa586aabfdd4a8ae
John Doe <ed78617@gmail.com>
2026-01-12 08:23:52 -0600

    add Leetcode 1266 (C++, easy, basic math)

---
+++ C++/1266-Minimum Time Visiting All Points.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int res = 0;
+        for (int i=1; i<points.size(); i++) {
+            int distX = abs(points[i][0] - points[i-1][0]);
+            int distY = abs(points[i][1] - points[i-1][1]);
+            res += max(distX, distY);
+        }
+
+        return res;
+    }
+};
