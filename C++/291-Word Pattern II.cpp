548db7abd126ddcd90b1754b2eb12aaacbabe053
John Doe <ed78617@gmail.com>
2026-07-09 15:21:22 -0500

    update Leetcode 291 (C++, hard, backtracking, use an unordered_map and an unordered_set to check bijective mapping, provide refactored code)

--- C++/291-Word Pattern II.cpp
+++ C++/291-Word Pattern II.cpp
@@ -2,38 +2,50 @@ class Solution {
 private:
     int Ns, Np;
 public:
-    bool dfs(int ind_s, int ind_p, unordered_map<char, string> &hmap, unordered_set<string> &wordSet, string &s, string &pattern) {
-        if (ind_s == Ns && ind_p == Np) {
+    bool dfs(int idx_p, int idx_s, string& pattern, string& s, unordered_map<char, string>& hmap, unordered_set<string>& used) {
+        if (idx_p == Np && idx_s == Ns) {
             return true;
-        } else if (ind_s == Ns || ind_p == Np) {
+        } else if (idx_p == Np || idx_s == Ns) {
             return false;
         }
 
-        for (int i=ind_s; i < Ns; i++) {
-            string tmp_s = s.substr(ind_s, i-ind_s+1);
-            if (hmap.find(pattern[ind_p]) == hmap.end()) {
-                if (wordSet.find(tmp_s) != wordSet.end()) continue;
-                hmap[pattern[ind_p]] = tmp_s;
-                wordSet.insert(tmp_s);
-                if (dfs(i+1, ind_p+1, hmap, wordSet, s, pattern)) {
-                    return true;
-                }
-                hmap.erase(pattern[ind_p]);
-                wordSet.erase(tmp_s);
-            } else {
-                if (hmap[pattern[ind_p]] != tmp_s) continue;
-                if (dfs(i+1, ind_p+1, hmap, wordSet, s, pattern)){
-                    return true;
-                }
+        char curr = pattern[idx_p];
+
+        if (hmap.contains(curr)) {
+            int sz = hmap[curr].size();
+            if (Ns-idx_s < sz || s.substr(idx_s, sz) != hmap[curr]) {
+                return false;
             }
+            return dfs(idx_p+1, idx_s + sz, pattern, s, hmap, used);
         }
+
+        string nxtString = "";
+
+        for (int j = idx_s; j < Ns; j++) {
+            if (Ns-idx_s < Np - idx_p) break;
+            nxtString.push_back(s[j]);
+            if (used.contains(nxtString)) continue;
+            
+            hmap[curr] = nxtString;
+            used.insert(nxtString);
+            if (dfs(idx_p+1, idx_s + nxtString.size(), pattern, s, hmap, used)){
+                return true;
+            }
+            used.erase(nxtString);
+            hmap.erase(curr);
+        }
+
         return false;
     }
     bool wordPatternMatch(string pattern, string s) {
+        this->Np = pattern.size();
+        this->Ns = s.size();
+        
         unordered_map<char, string> hmap;
-        unordered_set<string> wordSet;
-        Ns = s.size();
-        Np = pattern.size();
-        return dfs(0, 0, hmap, wordSet, s, pattern);
+        unordered_set<string> used;
+        
+        auto res = dfs(0, 0, pattern, s, hmap, used);
+
+        return res;
     }
 };
