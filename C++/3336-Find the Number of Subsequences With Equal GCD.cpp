fbcf301ce487f4451430f28cb07a99612b5de90f
John Doe <ed78617@gmail.com>
2026-07-15 14:54:22 -0500

    update Leetcode 3336 (C++, hard, DP, added iterative solution, note the way we set DP states, good problem)

--- C++/3336-Find the Number of Subsequences With Equal GCD.cpp
+++ C++/3336-Find the Number of Subsequences With Equal GCD.cpp
@@ -29,3 +29,47 @@ public:
         return dfs(nums, 0, mx+1, mx+1);
     }
 };
+
+
+// iterative
+class Solution {
+private:
+    static constexpr int kMod = 1e9+7;
+public:
+    int subsequencePairCount(vector<int>& nums) {
+        int N = nums.size();
+        int mx = *max_element(begin(nums), end(nums));
+
+        // dp[i][j] = ways to create seq1 with GCD i and seq2 with GCD j
+        vector<vector<int>> dp(mx+1, vector<int>(mx+1, 0));
+        dp[0][0] = 1;
+
+        for (int num:nums) {
+            vector<vector<int>> ndq(mx+1, vector<int>(mx+1, 0));
+            for (int i=0; i<=mx; i++) {
+                int gcd1 = __gcd(num, i);
+                for (int j=0; j<=mx; j++) {
+                    int val = dp[i][j];
+                    if (val == 0) {
+                        continue;
+                    }
+                    int gcd2 = __gcd(num, j);
+                    // add num to seq1
+                    ndq[gcd1][j] = (ndq[gcd1][j] + val)%kMod;
+                    // add num to seq2
+                    ndq[i][gcd2] = (ndq[i][gcd2] + val)%kMod;
+                    // skip
+                    ndq[i][j] = (ndq[i][j] + dp[i][j])%kMod;
+                }
+            }
+            swap(dp, ndq);
+        }
+
+        long long res = 0;
+        for (int g=1; g<=mx; g++) {
+            res = (res + dp[g][g])%kMod;
+        }
+
+        return res;
+    }
+};
