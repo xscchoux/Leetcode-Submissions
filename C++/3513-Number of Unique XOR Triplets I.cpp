a8b83390027c2f54226c81cbdfd3689f3207ae1d
John Doe <ed78617@gmail.com>
2026-07-23 14:54:53 -0500

    update 3513 (C++, hard, bit manipulation + some observation)

--- C++/3513-Number of Unique XOR Triplets I.cpp
+++ C++/3513-Number of Unique XOR Triplets I.cpp
@@ -3,13 +3,8 @@ public:
     int uniqueXorTriplets(vector<int>& nums) {
         int N = nums.size();
         if (N <= 2) return N;
-        
-        int OR = 0;
-        for (int num:nums) {
-            OR |= num;
-        }
 
-        int msb = log2(OR);  // we can get all permutations once we have the msb...based on observation
+        int msb = log2(N);  // we can get all permutations once we have the msb...based on observation
 
         return pow(2, msb+1);
     }
