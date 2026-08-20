85ebee38f72db9714e953a6455046f0681e8b333
John Doe <ed78617@gmail.com>
2026-08-20 06:11:20 -0500

    add Leetcode 3069 (C++, easy, just easy)

---
+++ C++/3069-Distribute Elements Into Two Arrays I.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> resultArray(vector<int>& nums) {
+        vector<int> arr1 = {nums[0]}, arr2 = {nums[1]};
+
+        for (int i=2; i<nums.size(); i++) {
+            if (arr1.back() > arr2.back()) {
+                arr1.push_back(nums[i]);
+            } else {
+                arr2.push_back(nums[i]);
+            }
+        }
+
+        arr1.insert(end(arr1), begin(arr2), end(arr2));
+
+        return arr1;
+    }
+};
