9ae3c0717da1a6d6ab9bc71e97359e32ce209170
John Doe <ed78617@gmail.com>
2026-08-28 16:16:02 -0500

    update Leetcode 3734 (C++, hard, backtracking, also added another slower solution using sequential enumeration, good problem)

--- C++/3734-Lexicographically Smallest Palindromic Permutation Greater Than Target.cpp
+++ C++/3734-Lexicographically Smallest Palindromic Permutation Greater Than Target.cpp
@@ -1,4 +1,5 @@
- class Solution {
+// backtracking
+class Solution {
 public:
     string res = "";
     char midChar = '\0';
@@ -100,5 +101,84 @@ public:
         backtracking(0, cnt, N/2, target, tmp, false);
 
         return res;
+    }
+};
+
+
+// Straightforward sequential enumeration (slower but easy to understand)
+class Solution {
+public:
+    int check(vector<int>& cnt, string& prefix, char added, string& mid, string& target) {
+        string left = prefix;
+        left.push_back(added);
+
+        for (int i=25; i>=0; i--) {
+            if (cnt[i] == 0) continue;
+            left.append(cnt[i], 'a' + i);
+        }
+
+        string reversed_left = left;
+        reverse(begin(reversed_left), end(reversed_left));
+        string palindrome = left + mid + reversed_left;
+
+        return palindrome > target;
+    }
+    string lexPalindromicPermutation(string s, string target) {
+        int N = s.size();
+        if (N == 1) {
+            return s > target? s: "";
+        }
+
+        vector<int> cnt(26, 0);
+        for (char c: s) {
+            cnt[c-'a']++;
+        }
+
+        bool hasOdd = false;
+        string mid = "";
+        for (int i=0; i<26; i++) {
+            if (cnt[i]&1) {
+                if (hasOdd) {
+                    return "";
+                }
+                mid = string(1, 'a' + i);
+                hasOdd = true;
+            }
+            cnt[i] /= 2;
+        }
+
+         // Construct the left part of each digit greedily
+        string prefix = "";
+        for (int i=0; i<N/2; i++) {
+            bool found = false;
+            for (int j=0; j<26; j++) {
+                if (cnt[j] == 0) continue;
+                cnt[j]--;
+                if (check(cnt, prefix, 'a' + j, mid, target)) {
+                    prefix.push_back('a'+j);
+                    found = true;
+                    break;
+                } else {
+                    cnt[j]++;
+                }
+            }
+
+            if (!found) return "";
+            if (prefix[i] > target[i]) {
+                for (int k=0; k<26; k++) {
+                    if (cnt[k] == 0) continue;
+                    prefix.append(cnt[k], 'a'+k);
+                }
+                string reversed_prefix = prefix;
+                reverse(begin(reversed_prefix), end(reversed_prefix));
+                return prefix + mid + reversed_prefix;
+            }
+        }
+
+         // Construct the final string
+        string reversed_prefix = prefix;
+        reverse(begin(reversed_prefix), end(reversed_prefix));
+        return prefix + mid + reversed_prefix;
+
     }
 };
