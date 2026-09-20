899f30d8b6a1e6fb28a0868d305550452eccccf4
John Doe <ed78617@gmail.com>
2026-09-20 14:29:43 -0500

    update Leetcode 1621 (C++, medium-hard, DP or cominatorics, added a fast solution using combinatorics, good problem)

--- C++/1621-Number of Sets of K Non-Overlapping Line Segments.cpp
+++ C++/1621-Number of Sets of K Non-Overlapping Line Segments.cpp
@@ -21,3 +21,41 @@ public:
         return (dp[n-1][1][k] + dp[n-1][0][k])%kMod;
     }
 };
+
+
+
+// Use combinatorics, very fast but kinda hard (add extra k-1 length-one segments)
+using LL = long long;
+constexpr int kMod = 1e9+7;
+
+class Solution {
+public:
+    LL powWithMod(LL a, LL b) {
+        a %= kMod;
+        LL res = 1;
+
+        while(b) {
+            if (b&1) res=(res*a)%kMod;
+            a=(a*a)%kMod;
+            b>>=1;
+        }
+
+        return res;
+    }
+    int numberOfSets(int n, int k) {
+        // add k-1 segments with length 1
+        // choose 2*k points from n+k-1 points, C(n+k-1, 2*k)
+        // Two points pair up as a segment. Remove k-1 length-one segments between each pair
+        // Note this problem has nothing to do with "stars and bars"
+
+        long long nom = 1, den = 1;
+
+        for (int d = min(2*k, n+k-1-2*k), cnt = 0; d>=1; d--, cnt++) {
+            nom = nom*(n+k-1-cnt)%kMod;
+            den = den*d%kMod;
+        }
+
+        return nom*powWithMod(den, kMod-2)%kMod;
+
+    }
+};
